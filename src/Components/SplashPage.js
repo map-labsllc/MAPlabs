506e36610b88df824886cd29a035803353a69aed
John Maddock <jmmaddock@gmail.com>
2019-01-18 14:58:08 -0700

    basic splash for under the nav

---
+++ src/Components/SplashPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SplashPage = () => {
+    render(
+        <div>
+            <h3>Welcome to MAPMaker</h3>
+            <h5>INSPIRE THYSELF</h5>
+            <button bsStyle="success" bsSize="large" href="/login">Log In</button> <button bsStyle="danger" bsSize="large">Sign Up</button>
+            <img src="https://cdn.pixabay.com/photo/2016/10/31/12/09/seashore-1785414_960_720.jpg" />
+            <text>What this website is about Lorem ipsum dolor amet meditation heirloom vape plaid, gluten-free trust fund yr tbh pickled. Distillery scenester 8-bit asymmetrical tacos enamel pin. Ethical typewriter four dollar toast migas you probably haven't heard of them. Cliche yr four dollar toast shabby chic listicle dreamcatcher kombucha freegan copper mug intelligentsia vegan synth franzen vice. Locavore trust fund tacos, leggings blue bottle occupy lyft pinterest. Prism yr adaptogen raw denim gluten-free street art organic selvage leggings keytar activated charcoal marfa salvia. Biodiesel narwhal farm-to-table ramps humblebrag.
+Oh. You need a little dummy text for your mockup? How quaint.
+I bet you’re still using Bootstrap too…</text>
+        </div>
+    )
+}
+
+export default SplashPage
