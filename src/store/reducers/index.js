eabab78939f44f289952658f738e219fc1269bd5
Ed Phelps <edphelps1@gmail.com>
2019-01-19 17:48:50 -0700

    added staticdataRD

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import AuthReducer from './AuthReducer'
 import answersRD from '../answers/reducer'
 import transitionsRD from '../transitions/reducer'
+import staticdataRD from '../staticdata/reducer'
 
 export default combineReducers({
   auth: AuthReducer,
   answersRD,
   transitionsRD,
+  staticdataRD,
 })
