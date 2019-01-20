dfe230a7b45d659c5b7c8845688e2f1325afaa63
Ed Phelps <edphelps1@gmail.com>
2019-01-19 17:13:59 -0700

    added transitionsRD

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import AuthReducer from './AuthReducer'
 import answersRD from '../answers/reducer'
+import transitionsRD from '../transitions/reducer'
 
 export default combineReducers({
   auth: AuthReducer,
   answersRD,
+  transitionsRD,
 })
