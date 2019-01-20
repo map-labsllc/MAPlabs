1cbc10b8d63ad3cba6109b339c0ac786d6a249ac
Ed Phelps <edphelps1@gmail.com>
2019-01-20 16:55:41 -0700

    Lookup previous answer form answersRD

--- src/Containers/NarrativeCT.js
+++ src/Containers/NarrativeCT.js
@@ -1,21 +1,36 @@
 import { connect } from 'react-redux'
 import Narrative from '../Components/Narrative'
+import { getAnswers } from '../store/answers/reducer'
+
 // import { countComments, getComments } from '../reducers/commentsRD'
 // import { getComments } from '../reducers/commentsRD'
 // import { voteUpAC, voteDownAC, addCommentAC } from '../actionCreators'
 
 /* *****************************************
    mapStateToProps()
+
+   passedProps:
+     question_code -- "50"  // needs to be converted to integer
+     question -- the question string
 ******************************************** */
 const mapStateToProps = (state, passedProps) => {
   console.log("NarrativeCT::mapStateToProps()");
-  const { question_code, question } = passedProps
-  console.log("question_code, question: ", question_code, question);
 
+  // get and validate question_code and question
+  let { question_code, question } = passedProps
+  question_code = parseInt(question_code, 10)
+  if (!question_code) throw new Error("missing or non-integer question code: ", passedProps.question_code)
+  console.log("question: ", question);
+
+  // lookup the previous answer, note: getAnswers() returns an array of answers
+  const answers = getAnswers(state.answersRD, question_code)
+  console.log(`getAnswers(${question_code}): `, answers);
+  if (1 < answers.length) throw new Error("more than one narrative answer: ", passedProps.question_code, answers)
+  const previousAnswer= answers[0] || ''
 
   return {
     question: passedProps.question,
-    previousAnswer: passedProps.previousAnswer,
+    previousAnswer: previousAnswer,
     // post: passedProps.post,
     // comments: getComments(state.commentsRD, passedProps.post.id),
  }
