4222ef756d4b0bd3e6a96ce63602a41a7d01a549
Ed Phelps <edphelps1@gmail.com>
2019-01-20 19:35:14 -0700

    connected to store and db

--- src/Components/Narrative.js
+++ src/Components/Narrative.js
@@ -11,43 +11,42 @@ import {
 /* **************************************************
    Narrative component
 
-   Displays a single question and multi-line textarea.
+   Displays a single question with multi-line textarea and Save button.
 
    props:
+     question_code -- integer
      question -- string with the question
      previousAnswer -- string with the previous answer
-     onSaveCB(newAnswer) -- callback when user clicks Save 
+     onSaveCB(newAnswer) -- callback for when user clicks Save
 ***************************************************** */
 export default class Narrative extends React.Component {
 
   state = {
     isDirty: false,
+
+    // controlled component
+    answer: this.props.previousAnswer,
   }
 
-  // load the textarea with previousAnswer
-  componentDidMount = () => {
-    console.log("Narrative::componentDidMount()");
-    const { previousAnswer } = this.props
-    console.log("-- previousAnswer: ", previousAnswer);
-    const elem = document.getElementById('answer')
-    if (previousAnswer) elem.value = previousAnswer
-  }
-
-  // set isDirty
+  // set isDirty and control answer field
   onChange = (e) => {
-    // console.log("Narrative::onChange");
-    this.setState({ isDirty: true })
+    console.log("Narrative::onChange(), e: ", e.target.value);
+    this.setState({
+      answer: e.target.value,
+      isDirty: true,
+    })
   }
 
   // Send newAnswer value back to Container to persist
   //   and update Save button to indicate control is no longer dirty
   onSubmit = (e) => {
-    console.log("Narrative::onclickSave");
+    console.log(`Narrative::onclickSave(): ${this.state.answer}`);
+    console.log("state: ", this.state);
     e.preventDefault()
-    const value = e.target.answer.value.trim()
-    console.log("value: ", value)
+    // const value = e.target.answer.value.trim()
+    // console.log("value: ", value)
     this.setState({ isDirty: false })
-    // this.props.onSaveCB(value)
+    this.props.onSaveCB(this.props.question_code, this.state.answer)
   }
 
   // render!
@@ -55,8 +54,8 @@ export default class Narrative extends React.Component {
     // console.log("Narrative::render()")
 
     // initialize
-    const { question, previousAnswer, onSaveCB } = this.props
-    const { isDirty } = this.state
+    const { question, onSaveCB } = this.props
+    const { isDirty, answer } = this.state
 
     return (
       <Form
@@ -64,13 +63,14 @@ export default class Narrative extends React.Component {
         id = "narrative-form"
       >
         <FormGroup>
-          <ControlLabel>{question}</ControlLabel>
+          <ControlLabel>&nbsp;&nbsp;{question}</ControlLabel>
           <FormControl
             componentClass = "textarea"
             onChange = {this.onChange}
+            value = {answer}
             placeholder = "Please enter an answer and click < Save >"
             id = "answer"
-            autoFocus />
+          />
         </FormGroup>
         <Button type = "submit" id = "save-button">{((isDirty) ? "Save" : "----")}</Button>
       </Form>
--- src/Containers/NarrativeCT.js
+++ src/Containers/NarrativeCT.js
@@ -1,10 +1,22 @@
 import { connect } from 'react-redux'
 import Narrative from '../Components/Narrative'
 import { getAnswers } from '../store/answers/reducer'
+import {
+  updateAnswersAC,
+  persistAnswersAC } from '../store/answers/actions'
 
-// import { countComments, getComments } from '../reducers/commentsRD'
-// import { getComments } from '../reducers/commentsRD'
-// import { voteUpAC, voteDownAC, addCommentAC } from '../actionCreators'
+/* *****************************************
+   onSave()
+
+   Save the new answer to store and persist it.
+
+   question_code -- integer
+   newAnswer -- string
+******************************************** */
+// const onSave = (question_code, newAnswer) => {
+//   console.log(`NarrativeCT::onSave(${question_code}, ${newAnswer})`);
+//
+// }
 
 /* *****************************************
    mapStateToProps()
@@ -22,24 +34,38 @@ const mapStateToProps = (state, passedProps) => {
   if (!question_code) throw new Error("missing or non-integer question code: ", passedProps.question_code)
   console.log("question: ", question);
 
-  // lookup the previous answer, note: getAnswers() returns an array of answers
+  // find previous answer,
+  //   Note: getAnswers() returns an array but narrative should have at most one answer
   const answers = getAnswers(state.answersRD, question_code)
   console.log(`getAnswers(${question_code}): `, answers);
   if (1 < answers.length) throw new Error("more than one narrative answer: ", passedProps.question_code, answers)
-  const previousAnswer= answers[0] || ''
+  const previousAnswer = answers[0] || ''
 
   return {
-    question: passedProps.question,
-    previousAnswer: previousAnswer,
-    // post: passedProps.post,
-    // comments: getComments(state.commentsRD, passedProps.post.id),
- }
+    question_code,
+    question,
+    previousAnswer,
+  }
 }
 
 /* *****************************************
    mapDispatchToProps()
 ******************************************** */
 const mapDispatchToProps = dispatch => ({
+
+  /* *****************************************
+     onSaveCB()
+
+     Save the new answer to store and persist it.
+
+     question_code -- integer
+     newAnswer -- string
+  ******************************************** */
+  onSaveCB: (question_code, newAnswer) => {
+    console.log(`NarrativeCT::onSave(${question_code}, ${newAnswer})`);
+    dispatch(updateAnswersAC(question_code, [newAnswer]))
+    dispatch(persistAnswersAC(question_code, [newAnswer]))
+  }
   // voteUpCB: (post) => dispatch(voteUpAC(post)),
   // voteDownCB: (post) => dispatch(voteDownAC(post)),
   // addCommentCB: (comment) => dispatch(addCommentAC(comment)),
