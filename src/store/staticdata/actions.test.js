720094f216ba8313c7eb0ec5a486e0c070115043
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-22 14:14:48 -0700

    only test action creater

--- src/store/staticdata/actions.test.js
+++ src/store/staticdata/actions.test.js
@@ -8,41 +8,47 @@ import {
     STATICDATA_ERROR_DB,
 } from './constants'
 
-import {
-    loadstaticJSON,
-    loadAllStaticdataAC   
-} from './actions'
+import { loadAllStaticdataAC } from './actions'
 
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
 const URL = `end:.json`
 const DATA = { section: 'TEST', jsonData: ['TEST']}
+const BELIEFS_FN = 'beliefs'
+const LIFEDESCRS_FN = 'lifedescrs'
+const STRENGTHS_FN = 'strengths'
 
 describe('async actions', () => {
   afterEach(() => {
     fetchMock.restore()
   })
-
-  describe('loadstaticJSON', () => {
-    it('returns object with section and jsonData when fetching was successful', async () => {
+  describe('loadAllStaticdataAC', () => {
+    it('creates STATICDATA_LOAD when fetching was successful', async () => {
         fetchMock.get(URL, DATA.jsonData)
-        const expectedActions = []
+        const expectedActions = [
+            { type: STATICDATA_LOADING },
+            { type: STATICDATA_LOAD, payload: {
+                [BELIEFS_FN]: DATA.jsonData,
+                [LIFEDESCRS_FN]: DATA.jsonData,
+                [STRENGTHS_FN]: DATA.jsonData
+            }}
+        ]
         const store = mockStore()
 
-        const expectedReturn = await loadstaticJSON(store.dispatch, DATA.section)
+        await store.dispatch(loadAllStaticdataAC())
 
         expect(store.getActions()).toEqual(expectedActions)
-        expect(expectedReturn).toEqual(DATA)
     }),
-    it('creates STATICDATA_ERROR_DB when fetching answers was not successful', async () => {
+    it('creates STATICDATA_ERROR_DB when fetching was not successful', async () => {
         const error = new Error('Fetch failed')
         fetchMock.get(URL, { throws: error })
         const expectedActions = [
+            { type: STATICDATA_LOADING },
             { type: STATICDATA_ERROR_DB, payload: error }
         ]
         const store = mockStore()
 
-        await loadstaticJSON(store.dispatch, DATA.section)
+        await store.dispatch(loadAllStaticdataAC())        
 
         expect(store.getActions()).toEqual(expectedActions)
     })
