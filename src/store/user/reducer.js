ba2b773bfe181e43b128118959d068848d8b351c
Ed Phelps <edphelps1@gmail.com>
2019-01-23 10:11:01 -0700

    mod/sec_complete -> curr_module/section

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -17,8 +17,8 @@ import {
       email: "tharrison@gmail.com",
       login_service_id: 1,
       login_token: "DFDS34543GD",
-      mod_complete: 0,
-      sec_complete: 0,
+      curr_module: 1,
+      curr_section: 1,
       created_at: 2019-01-15 04:00:00-07
      },
   }
