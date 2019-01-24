ebcda8d5b0aeaf9b354268ce20c5052c7816b36c
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-24 11:58:34 -0700

    link to login

--- src/Components/SplashPage.js
+++ src/Components/SplashPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Col, ButtonToolbar } from 'react-bootstrap'
+import { Button, ButtonToolbar } from 'react-bootstrap'
 
 import aboutText from './aboutText'
 const SplashPage = () => {
@@ -9,7 +9,7 @@ const SplashPage = () => {
             <h4 style={style.subtitle} className="text-center">The Science of Meaning and Purpose</h4>
             <ButtonToolbar style={style.buttonCentering} className='text-center'>
                 <Button style={style.button}>Sign Up</Button>
-                <Button style={style.button}>Log In</Button>
+                <Button style={style.button} href='/login'>Log In</Button>
             </ButtonToolbar>
             <img className="img-responsive center-block" src="https://scontent-dfw5-2.cdninstagram.com/vp/9f0a7c7aa18b83e0d11bf81408c5cbb3/5CF94F3C/t51.2885-15/sh0.08/e35/s750x750/46038013_203180027229315_5370813539263890085_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com" />
             <br />
