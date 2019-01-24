3e16736f3332e89dead34e90bb29ba3e8ec4f169
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-24 11:48:41 -0700

    move text to its own file

--- src/Components/SplashPage.js
+++ src/Components/SplashPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button, Col, ButtonToolbar } from 'react-bootstrap'
+
+import aboutText from './aboutText'
 const SplashPage = () => {
     return (
         <div style={style.background}>
@@ -12,11 +14,7 @@ const SplashPage = () => {
             <img className="img-responsive center-block" src="https://scontent-dfw5-2.cdninstagram.com/vp/9f0a7c7aa18b83e0d11bf81408c5cbb3/5CF94F3C/t51.2885-15/sh0.08/e35/s750x750/46038013_203180027229315_5370813539263890085_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com" />
             <br />
             
-            <h5 style={style.textMargin}>
-At MAPlabs, we turn the latest scientific findings on human meaning and purpose into programs that inform, empower, and fulfill human lives.
-When our lives have meaning and purpose we flourish. But when meaning and purpose lag, we passively endure the life we have rather than actively strive for the life that we want.
-That’s why we founded MAPlabs. Our research-based programs help you to create a richer life by improving your personal sense of meaning, and by developing a pathway toward a more purposeful life.
-If you are ready to build a life of deep meaning and clarifying purpose, then you are ready for MAPlabs.</h5>
+            <h5 style={style.textMargin}>{aboutText}</h5>
         </div >
     )
 }
