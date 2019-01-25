922781e6f0efad05d6a32edb786f243b02e4d0d0
Ed Phelps <edphelps1@gmail.com>
2019-01-25 12:34:46 -0700

    added /ed route

--- src/router.js
+++ src/router.js
@@ -5,21 +5,30 @@ import Login from './Components/login'
 
 import SignUp from './Components/SignUp'
 import ModulesPage from './Components/modulesPage'
+import TestModule from './Components/TestModule'
+
 export default class RouterComponent extends React.Component {
   render() {
       return (
-
-
-            <Switch>
-              <Route exact path={['/', '/splash']} component={SplashPage} initial />
-              <Route exact path ='/login' component = { Login }/>
-              <Route exact path ='/signup' component = { SignUp }/>
-              <Route exact path ='/modules/1' component = { ModulesPage }/>
-
-            </Switch>
-
-
+        <Switch>
+          <Route exact path={['/', '/splash']} component={SplashPage} initial />
+          <Route exact path ='/login' component = { Login }/>
+          <Route exact path ='/ed' component = { TestModule }/>
+          <Route exact path ='/signup' component = { SignUp }/>
+          <Route exact path ='/modules/1' component = { ModulesPage }/>
+        </Switch>
       )
     }
   }
 
+  // <TestTransitions />
+
+  // <TestModule />
+
+  // <Switch>
+  //   <Route exact path={['/', '/splash']} component={SplashPage} initial />
+  //   <Route exact path ='/login' component = { Login }/>
+  //   <Route exact path ='/signup' component = { SignUp }/>
+  //   <Route exact path ='/modules/1' component = { ModulesPage }/>
+  //
+  // </Switch>
