4725872bd6a53fa7b42913a1f66f1934cd8cfd27
Nick Griffen <nicholas.s.griffen@gmail.com>
2019-01-25 12:43:02 -0700

    Update router.js
    I'd ask we don't commit changes to the router when they're made for testing - keep those local and keep router on dev branch in a clean state. When you're done testing, do a git checkout development -- ./src/router.js.  
    If you need to access a different version you can check it out from another branch or previous commit but the more this file is in flux and we're adding routes and saving them, the more cleanup we have to do later.

--- src/router.js
+++ src/router.js
@@ -6,31 +6,15 @@ import Login from './Components/login'
 import SignUp from './Components/SignUp'
 import ModulesPage from './Components/modulesPage'
 
-import TestModule from './Components/TestModule'
-
-
 export default class RouterComponent extends React.Component {
   render() {
       return (
         <Switch>
-          <Route exact path={['/', '/splash']} component={SplashPage} initial />
+          <Route exact path='/' component={ SplashPage }/>
           <Route exact path ='/login' component = { Login }/>
-          <Route exact path ='/ed' component = { TestModule }/>
           <Route exact path ='/signup' component = { SignUp }/>
           <Route exact path ='/modules/1' component = { ModulesPage }/>
         </Switch>
       )
     }
   }
-
-  // <TestTransitions />
-
-  // <TestModule />
-
-  // <Switch>
-  //   <Route exact path={['/', '/splash']} component={SplashPage} initial />
-  //   <Route exact path ='/login' component = { Login }/>
-  //   <Route exact path ='/signup' component = { SignUp }/>
-  //   <Route exact path ='/modules/1' component = { ModulesPage }/>
-  //
-  // </Switch>
