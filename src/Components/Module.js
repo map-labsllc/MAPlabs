dd236608220a9196fb9c4f129ced7077536a264f
Ed Phelps <edphelps1@gmail.com>
2019-01-25 18:51:44 -0700

    readding files to github

---
+++ src/Components/Module.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Section from '../Components/Section'
+import {
+  Button,
+  Checkbox,
+  Col,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+} from 'react-bootstrap';
+
+/* **************************************************
+   Module
+
+   Shows a module
+
+   props:
+     moduleNum -- integer, the module number
+     moduleTitle -- title of the Module
+     moduleDescription -- could be many lines, is we need paragraphs then will need to set innerHTML
+     children -- the Section components to display
+***************************************************** */
+export default class Module extends React.Component {
+
+  render() {
+    console.log("Module::render()")
+
+    let { moduleNum, moduleTitle, moduleDescription, children } = this.props
+
+    return (
+      <>
+        <h3>Module: {moduleNum} - {moduleTitle}</h3>
+        <p><i>Module Description: {moduleDescription}</i></p>
+        {children}
+        <p> </p>
+      </>
+    )
+  }
+}
+
+// <Section moduleNum = {moduleNum} sectionNum = "1" sectionTitle = "Section One" />
---
+++ src/Components/Narrative.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import {
+  Button,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+} from 'react-bootstrap'
+
+/* **************************************************
+   Narrative component
+
+   Displays a single question with:
+     -- multi-line textarea
+     -- Save button
+
+   props:
+     userId -- integer
+     question -- { code: 50, text: "Question 50" }
+     prompts -- [] or array of strings with short answers saved by previous exercise
+     instructions -- can be empty string
+     previousAnswer -- string with the previous answer
+     onPersistCB(newAnswer) -- callback for when user clicks Save
+     onCloseModalCB -- call to close the modal this control resides in
+***************************************************** */
+export default class Narrative extends React.Component {
+
+  state = {
+    isDirty: false,
+
+    // controlled component
+    answer: this.props.previousAnswer,
+  }
+
+  /* ******************************************************** */
+  // set isDirty and controlled answer field
+  onChange = ( e ) => {
+    console.log( "Narrative::onChange(), e: ", e.target.value )
+    this.setState( {
+      isDirty: true,
+      answer: e.target.value,
+    } )
+  }
+
+  /* ******************************************************** */
+  // update store and persist the value as user could be clicking outside Modal and shitting it down
+  onBlur = () => {
+    console.log( "Narrative::onBlur()" )
+    this.updateAndPersist()
+  }
+
+  /* ******************************************************** */
+  // helper, update the store and persist
+  updateAndPersist = () => {
+    console.log( "state: ", this.state )
+
+    this.setState( { isDirty: false } )
+
+    const { onPersistCB, onCloseModalCB, userId } = this.props
+    const { answer } = this.state
+
+    onPersistCB( userId, answer )
+    onCloseModalCB()
+  }
+
+  /* ******************************************************** */
+  // Send newAnswer value back to Container to persist
+  //   and update Save button to indicate control is no longer dirty
+  onSubmit = ( e ) => {
+    console.log( `Narrative::onclickSave(): ${this.state.answer}` )
+    console.log( "state: ", this.state )
+    e.preventDefault()
+    this.updateAndPersist()
+  }
+
+  /* ******************************************************** */
+  // render!
+  render() {
+    // console.log("Narrative::render()")
+
+    // initialize
+    const { question, instructions } = this.props
+    const { answer } = this.state
+
+    return (
+      <Form onSubmit={this.onSubmit} >
+        <FormGroup>
+          {instructions && (
+            <p>..Narrative instructions: <i>{instructions}</i></p>
+          )}
+          <ControlLabel>&nbsp;&nbsp;{question.text}</ControlLabel>
+          <FormControl
+            componentClass = "textarea"
+            onChange = {this.onChange}
+            onBlur = {this.onBlur}
+            value = {answer}
+            placeholder = "Please enter an answer and click Close"
+          />
+        </FormGroup>
+        <Button type = "submit">Close</Button>
+      </Form>
+    )
+  }
+}
