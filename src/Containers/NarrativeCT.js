1b3fb53925eb111e7d70b679675a83a4638c0c13
Ed Phelps <edphelps1@gmail.com>
2019-01-25 17:21:52 -0700

    adding prompts

--- src/Containers/NarrativeCT.js
+++ src/Containers/NarrativeCT.js
@@ -10,23 +10,33 @@ import {
    mapStateToProps()
 
    passedProps:
-     question -- { code: 50, text: "question 50" }
-     instructions -- string with instructions, can be an empty string for no instructions
-     onCloseModalCB -- call to close the modal this control resides in
+    promptQuestionCode -- integer, 0 or questionCode to use to grab previousAnswers
+                          from answersRD to display as a prompt for the Narrative
+    question -- { code: 50, text: "question 50" }
+    instructions -- string with instructions, can be an empty string for no instructions
+    onCloseModalCB -- call to close the modal
 ******************************************** */
 const mapStateToProps = ( state, passedProps ) => {
   console.log(  "NarrativeCT::mapStateToProps(  )"  )
 
-  const { question, instructions, onCloseModalCB } = passedProps
+  const { promptQuestionCode, question, instructions, onCloseModalCB } = passedProps
   if ( !question.code ) throw new Error( "missing question code: ", passedProps.question_code )
 
   // get userId
   const userId = getUser( state.userRD ).user_id
 
+  // find prompts
+  const prompts = []
+  const promptQuestionCodeNumeric = parseInt( promptQuestionCode, 10 )
+  if (promptQuestionCodeNumeric) {
+    prompts = getAnswers( state.answersRD, promptQuestionCodeNumeric )
+  }
+  console.log("Prompts for narrative: ", prompts);
+
   // find previous answer, if any
   //   Note: getAnswers() returns an array but narrative should have at most one answer
-  // const answers = getAnswers( state.answersRD, question.code )
-  const answers = state.answersRD.questions[question.code] || []
+  // const answers = state.answersRD.questions[question.code] || []
+  const answers = getAnswers( state.answersRD, question.code )
   console.log( `getAnswers( ${question.code} ): `, answers )
   if ( 1 < answers.length ) throw new Error( "more than one narrative answer: ", passedProps.question_code, answers )
   const previousAnswer = answers[0] || ''
