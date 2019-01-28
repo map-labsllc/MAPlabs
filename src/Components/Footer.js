1966120148c03acfd159a04006ec1421dc123efc
John Maddock <jmmaddock@gmail.com>
2019-01-28 11:26:15 -0700

    started Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <nav className="navbar navbar-dark">
+      <div></div>
+      <div className="navbar-brand" href="/">&copy; 2019</div>
+    </nav>
+  )
+}
+
+export default Footer
