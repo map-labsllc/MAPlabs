e4a317cab0523f461758afba1dde9147ca0b4e44
Ed Phelps <edphelps1@gmail.com>
2019-01-29 12:38:05 -0700

    added files to deleted dir

---
+++ src/Components/delete/Narrative 2.js
@@ -0,0 +1,127 @@
+import React from 'react'
+import {
+  Button,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+} from 'react-bootstrap'
+import Prompts from './Prompts'
+
+/* **************************************************
+   Narrative component
+
+   Displays a single question with:
+     -- multi-line textarea
+     -- Save button
+
+   props:
+     userId -- integer
+     question -- { code: 50, text: "Question 50" }
+     prompts -- [] or array of strings with short answers saved by previous exercise
+     instructions -- can be empty string
+     previousAnswer -- string with the previous answer
+     isDynamic -- undefined or true
+                  rendering static version in Popup or dynamic verison in Modal     onPersistCB(newAnswer) -- callback for when user clicks Save
+     onCloseModalCB -- call to close the modal this control resides in
+***************************************************** */
+export default class Narrative extends React.Component {
+
+  state = {
+    isDirty: false,
+
+    // controlled component
+    answer: this.props.previousAnswer,
+  }
+
+  /* ******************************************************** */
+  // set isDirty and controlled answer field
+  onChange = (e) => {
+    console.log("Narrative::onChange(), e: ", e.target.value)
+    this.setState({
+      isDirty: true,
+      answer: e.target.value,
+    })
+  }
+
+  /* ******************************************************** */
+  // update store and persist the value as user could be clicking outside Modal and shitting it down
+  onBlur = () => {
+    console.log("Narrative::onBlur()")
+    this.updateAndPersist()
+  }
+
+  /* ******************************************************** */
+  // helper, update the store and persist
+  updateAndPersist = () => {
+    console.log("state: ", this.state)
+
+    this.setState({ isDirty: false })
+
+    const { onPersistCB, onCloseModalCB, userId } = this.props
+    const { answer } = this.state
+
+    onPersistCB(userId, answer)
+    onCloseModalCB()
+  }
+
+  /* ******************************************************** */
+  // Send newAnswer value back to Container to persist
+  //   and update Save button to indicate control is no longer dirty
+  onSubmit = (e) => {
+    console.log(`Narrative::onclickSave(): ${this.state.answer}`)
+    console.log("state: ", this.state)
+    e.preventDefault()
+    this.updateAndPersist()
+  }
+
+  /* ******************************************************** */
+  // render!
+  render() {
+    // console.log("Narrative::render()")
+
+    // initialize
+    const { question, prompts, instructions, isDynamic } = this.props
+    const { answer } = this.state
+
+    if (!isDynamic) {
+      return (
+        <p>Previous answer: {answer}</p>
+      )
+    }
+
+    return (
+      <>
+        <Prompts prompts = {prompts}/>
+
+        <Form onSubmit={this.onSubmit} >
+          <FormGroup>
+            {instructions && (
+              <p>..Narrative instructions: <i>{instructions}</i></p>
+            )}
+            <ControlLabel>&nbsp;&nbsp;{question.text}</ControlLabel>
+            <FormControl
+              componentClass="textarea"
+              onChange={this.onChange}
+              onBlur={this.onBlur}
+              value={answer}
+              placeholder="Please enter an answer and click Close"
+              bsSize="large"
+            />
+          </FormGroup>
+          <div>
+            <Button type="submit" style={style.closeButton}>Close</Button>
+          </div>
+        </Form>
+      </>
+    )
+  }
+}
+const style = {
+  closeButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+
+  }
+}
---
+++ src/Components/delete/questionsList 2.js
@@ -0,0 +1,233 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+//import { bindActionCreators } from 'redux'
+import { Button } from 'react-bootstrap'
+import LifeDescriptor from './lifeDescriptor'
+import Arrows from './Arrows'
+import { getUser } from '../store/user/reducer'
+//import { loadAllAnswersAC } from '../store/answers/actions'
+//import { getAnswers } from '../store/answers/reducer'
+import {
+  updateAnswersAC,
+  persistAnswersAC
+} from '../store/answers/actions'
+import '../CSS/ModalNavButtons.css'
+
+const NUM_PER_PAGE = 5
+
+class QuestionsList extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      currentIndex: 0,
+      page: 0,
+      selections: [],
+      persistingArray: [],
+    }
+  }
+
+  previousSlide = (arr) => e => {
+    const lastIndex = arr.length
+    const { currentIndex } = this.state
+    const shouldResetIndex = currentIndex === 0
+    const index = shouldResetIndex ? lastIndex : currentIndex - 1
+    const page = this.state.page > 0 ? this.state.page - 1 : lastIndex - 1
+
+    this.setState({
+      currentIndex: index,
+      page
+    })
+
+  }
+
+  nextSlide = (arr) => e => {
+    const lastIndex = arr.length
+    const { currentIndex } = this.state
+    const shouldResetIndex = currentIndex === lastIndex
+    const index = shouldResetIndex ? 0 : currentIndex + 1
+    const page = this.state.page < lastIndex - 1 ? this.state.page + 1 : 0
+
+    this.setState({
+      currentIndex: index,
+      page
+    })
+  }
+
+  splittingArray = (arr) => {
+    var size = NUM_PER_PAGE
+    var arrayOfArrays = []
+    for (var i = 0; i < arr.length; i += size) {
+      arrayOfArrays.push(arr.slice(i, i + size))
+    }
+
+    return arrayOfArrays
+  }
+
+
+  addToPersistingArray = (field1, action, field2, cb) => {
+
+    const sentence = field1 + action + field2
+
+    this.setState({ persistingArray: [...this.state.persistingArray, sentence] })
+    console.log(this.state.persistingArray)
+  }
+
+  buildingCongruentSentence = (lifedescriptor, aOrB) => {
+
+    let structured = lifedescriptor.description.split('#')
+    let first = structured[0]
+    let second = structured[1]
+    const sentence = first + (aOrB === 'a' ? lifedescriptor.a : lifedescriptor.b) + second
+    return sentence
+
+  }
+  render() {
+
+    const { isDynamic, lifeDescriptors, isLoading, userId, onPersistCB, onCloseModalCB } = this.props
+
+    let pages = this.splittingArray(lifeDescriptors)
+    let list = pages.map((element, pageNum) => (
+      element.map((ele, i) => {
+        const idxLifedescriptions = i + pageNum * NUM_PER_PAGE
+        return (
+          <LifeDescriptor
+            key={idxLifedescriptions}
+            data={ele}
+            showCheckedA={
+              () => {
+                const newArr = this.state.selections
+                newArr[idxLifedescriptions] = "a"
+                this.setState({
+                  clickedA: 'success',
+                  selections: newArr
+                })
+              }}
+            showCheckedB={
+              () => {
+                const newArr = this.state.selections
+                newArr[idxLifedescriptions] = "b"
+                this.setState({
+                  clickedB: 'success',
+                  selections: newArr
+                })
+                console.log('ed says put a title', newArr)
+              }
+            }
+            checkedA={this.state.selections[idxLifedescriptions] === 'a'}
+            checkedB={this.state.selections[idxLifedescriptions] === 'b'}
+            addingData={this.addToPersistingArray}
+          />
+
+        )
+      })
+    ))
+
+    /* *************************************** */
+    if (!isDynamic) {
+      const sentences = []
+      const { lifeDescriptors } = this.props
+      for (let i = 0; i < lifeDescriptors.length; i++) {
+        if (this.state.selections[i]) {
+          let sentence = this.buildingCongruentSentence(lifeDescriptors[i], this.state.selections[i])
+          sentences.push(sentence)
+        }
+      }
+
+      return (
+        <>
+          {sentences.map((sentence, idx) => (
+            <p key={idx}>{sentence.text}</p>
+          ))}
+        </>
+      )
+    }
+
+    return (
+      <>
+        <p>{((isLoading) ? "loading...." : "")}</p>
+        {!isLoading && (
+          <>
+            <p>{((isLoading) ? "loading...." : "")}</p>
+            {!isLoading && (
+              <>
+
+                {list[this.state.page]}
+                <br />
+                <Arrows
+                  direction="left"
+                  clickFunction={this.previousSlide(pages)}
+                  glyph="arrow-left"
+                  className="previousButton"
+                />
+                <Button
+                  className="closeButton"
+                  onClick={() => {
+                    const sentences = []
+
+                    for (let i = 0; i < lifeDescriptors.length; i++) {
+                      if (this.state.selections[i]) {
+                        let sentence = this.buildingCongruentSentence(lifeDescriptors[i], this.state.selections[i])
+                        sentences.push(sentence)
+
+                      }
+                    }
+                    onPersistCB(userId, sentences)
+                    onCloseModalCB()
+                  }}>Close</Button>
+                <Arrows
+                  className="nextButton"
+                  direction="right"
+                  clickFunction={this.nextSlide(pages)}
+                  glyph="arrow-right"
+                />
+              </>
+            )}
+          </>
+
+        )}
+      </>
+    )
+  }
+}
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+// Wrap in container to get access to store and dispatch
+const mapStateToProps = (state, passedProps) => {
+  console.log('state', state)
+  const { isDynamic, question, instructions, onCloseModalCB } = passedProps
+  return {
+    isLoading: state.answersRD.isLoading || state.staticdataRD.isLoading,
+    userId: getUser(state.userRD).user_id,
+    question,
+    instructions,
+    isDynamic,
+    onCloseModalCB,
+    lifeDescriptors: state.staticdataRD.lifeDescriptions,
+    persistant_array: state.persistant_array
+  }
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+const mapDispatchToProps = (dispatch, passedProps) => {
+  const onPersist = (userId, lifeDescriptionsArray) => {
+    const { question } = passedProps
+
+    dispatch(updateAnswersAC(question.code, lifeDescriptionsArray))
+    dispatch(persistAnswersAC(userId, question.code, lifeDescriptionsArray))
+  }
+  return {
+    onPersistCB: onPersist
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(QuestionsList)
