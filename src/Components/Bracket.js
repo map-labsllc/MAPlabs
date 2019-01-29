dae04abe7054d775eaa7fae6eb1e2db5ce062fd1
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-29 14:22:39 -0700

    export Bracket, require prompts proptype

---
+++ src/Components/Bracket.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Bracket() {
+
+} 
+
+Bracket.propTypes = {
+    prompts: PropTypes.arrayOf(PropTypes.string).isRequired
+}
