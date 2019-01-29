0d35229ad36c6fe665038872b74389f41467e088
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-29 11:32:16 -0700

    describe prop tests

---
+++ src/Components/Bracket.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import sinon from 'sinon'
+
+import Bracket from './Bracket'
+
+Enzyme.configure({ adapter: new Adapter() })
+chai.use(chaiEnzyme())
+
+const shallow = Enzyme.shallow
+const { expect } = chai 
+
+const props = {
+    prompts: ['PropTypes.arrayOf(PropTypes.string).isRequired'],
+    question: {
+        code: Math.random(),
+        text: 'PropTypes.string.isRequired'
+    },
+    onUpdateStoreCB: () => {}
+}
+
+describe('<Bracket />', () => {
+    let stub 
+    beforeAll(() => {
+        stub = sinon.stub(console, 'error')
+        stub.throwsArg(0)
+    })
+    afterEach(() => {
+        stub.resetHistory()
+    }) 
+
+    it('requires a prompts prop', () => {
+        expect(() => shallow(<Bracket />)).to.throw('Warning: Failed prop type: The prop `prompts` is marked as required')
+    })
+    it('requires a question prop', () => {
+        expect(() => shallow(<Bracket 
+                                prompts={props.prompts}/>)).to.throw('Warning: Failed prop type: The prop `question` is marked as required')
+    })
+    it('requires an onUpdateStoreCB prop', () => {
+        expect(() => shallow(<Bracket 
+                                prompts={props.prompts} 
+                                question={props.question}/>)).to.throw('Warning: Failed prop type: The prop `onUpdateStoreCB` is marked as required')
+    })
+})
