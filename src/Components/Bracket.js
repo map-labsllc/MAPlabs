4154ff353ba6ed5c0613663aa7ec4de12f2e1a8e
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-30 11:03:43 -0700

    lint

--- src/Components/Bracket.js
+++ src/Components/Bracket.js
@@ -2,44 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default class Bracket extends React.Component {
-    constructor(props) {
-        super(props)
+    constructor( props ) {
+        super( props )
         this.state = {
             prompts: this.props.prompts.slice()
         }
     }
 
     updateStore = prompt => {
-        this.props.onUpdateStoreCB(prompt)
+        this.props.onUpdateStoreCB( prompt )
     }
 
     promptClick = index => e => {
-        if (this.state.prompts.length === 2) {
-            return this.updateStore(this.state.prompts[index])
+        if ( this.state.prompts.length === 2 ) {
+            return this.updateStore( this.state.prompts[index] )
         }
 
         const newPrompts = this.state.prompts.slice()
         
-        newPrompts.splice(index, 1)
+        newPrompts.splice( index, 1 )
 
-        return this.setState({
+        return this.setState( {
             prompts: newPrompts 
-        })
+        } )
     }
 
     render() {
-        const { question: {code, text} } = this.props
+        const { question: { code, text } } = this.props
         return (
             <div>
                 <p id={'question' + code}>{text}</p>
                 <p className='prompts'>
                     <span
                         id='prompt1' 
-                        onClick={this.promptClick(1)}
+                        onClick={this.promptClick( 1 )}
                         value={this.state.prompts[0]}>{this.state.prompts[0]}</span>
                     <span 
                         id='prompt2'
-                        onClick={this.promptClick(0)}
+                        onClick={this.promptClick( 0 )}
                         value={this.state.prompts[1]}>{this.state.prompts[1]}</span>
                 </p>
             </div>
@@ -48,10 +48,10 @@ export default class Bracket extends React.Component {
 } 
 
 Bracket.propTypes = {
-    prompts: PropTypes.arrayOf(PropTypes.string).isRequired,
-    question: PropTypes.shape({
+    prompts: PropTypes.arrayOf( PropTypes.string ).isRequired,
+    question: PropTypes.shape( {
         code: PropTypes.number.isRequired, 
         text: PropTypes.string.isRequired
-    }).isRequired,
+    } ).isRequired,
     onUpdateStoreCB: PropTypes.func.isRequired
 }
--- src/Components/Bracket.test.js
+++ src/Components/Bracket.test.js
@@ -7,8 +7,8 @@ import sinon from 'sinon'
 
 import Bracket from './Bracket'
 
-Enzyme.configure({ adapter: new Adapter() })
-chai.use(chaiEnzyme())
+Enzyme.configure( { adapter: new Adapter() } )
+chai.use( chaiEnzyme() )
 
 const shallow = Enzyme.shallow
 const { expect } = chai 
@@ -16,78 +16,78 @@ const { expect } = chai
 const props = {
     prompts: ['Rich/Poor', 'Happy/Sad', 'Love/Hate'],
     question: {
-        code: Math.floor(Math.random() * 100),
+        code: Math.floor( Math.random() * 100 ),
         text: 'How do you feel?'
     },
     onUpdateStoreCB: () => {}
 }
 
-describe('<Bracket />', () => {
+describe( '<Bracket />', () => {
     let stub 
     
-    beforeAll(() => {
-        stub = sinon.stub(console, 'error')
-        stub.throwsArg(0)
-    })
-    afterEach(() => {
+    beforeAll( () => {
+        stub = sinon.stub( console, 'error' )
+        stub.throwsArg( 0 )
+    } )
+    afterEach( () => {
         stub.resetHistory()
-    }) 
+    } ) 
 
-    it('requires a prompts prop', () => {
-        expect(() => shallow(<Bracket />)).to.throw('Warning: Failed prop type: The prop `prompts` is marked as required')
-    })
-    it('requires a question prop', () => {
-        expect(() => shallow(<Bracket 
-                                prompts={props.prompts}/>)).to.throw('Warning: Failed prop type: The prop `question` is marked as required')
-    })
-    it('requires an onUpdateStoreCB prop', () => {
-        expect(() => shallow(<Bracket 
+    it( 'requires a prompts prop', () => {
+        expect( () => shallow( <Bracket /> ) ).to.throw( 'Warning: Failed prop type: The prop `prompts` is marked as required' )
+    } )
+    it( 'requires a question prop', () => {
+        expect( () => shallow( <Bracket 
+                                prompts={props.prompts}/> ) ).to.throw( 'Warning: Failed prop type: The prop `question` is marked as required' )
+    } )
+    it( 'requires an onUpdateStoreCB prop', () => {
+        expect( () => shallow( <Bracket 
                                 prompts={props.prompts} 
-                                question={props.question}/>)).to.throw('Warning: Failed prop type: The prop `onUpdateStoreCB` is marked as required')
-    })
-    it('renders question.text in a p with id question.code',  () => {
-        const wrapper = shallow(<Bracket {...props}/>)
-        expect(wrapper.find('p#question' + props.question.code)).to.have.lengthOf(1)
-    })
-    it('renders two children in a p with prompts class',  () => {
-        const wrapper = shallow(<Bracket {...props}/>)
-        expect(wrapper.find('p.prompts').children()).to.have.lengthOf(2)
-    })
-    it('handles click on first prompt and re-renders with second prompt replaced',  () => {
-        const wrapper = shallow(<Bracket {...props}/>)
-        const prompts = wrapper.find('.prompts')
+                                question={props.question}/> ) ).to.throw( 'Warning: Failed prop type: The prop `onUpdateStoreCB` is marked as required' )
+    } )
+    it( 'renders question.text in a p with id question.code',  () => {
+        const wrapper = shallow( <Bracket {...props}/> )
+        expect( wrapper.find( 'p#question' + props.question.code ) ).to.have.lengthOf( 1 )
+    } )
+    it( 'renders two children in a p with prompts class',  () => {
+        const wrapper = shallow( <Bracket {...props}/> )
+        expect( wrapper.find( 'p.prompts' ).children() ).to.have.lengthOf( 2 )
+    } )
+    it( 'handles click on first prompt and re-renders with second prompt replaced',  () => {
+        const wrapper = shallow( <Bracket {...props}/> )
+        const prompts = wrapper.find( '.prompts' )
         
-        const firstChild = prompts.childAt(0)
-        const secondChild = prompts.childAt(1)
+        const firstChild = prompts.childAt( 0 )
+        const secondChild = prompts.childAt( 1 )
 
         //check initial values of children
-        expect(firstChild).to.have.value(props.prompts[0])
-        expect(secondChild).to.have.value(props.prompts[1])
+        expect( firstChild ).to.have.value( props.prompts[0] )
+        expect( secondChild ).to.have.value( props.prompts[1] )
         
         //click first child 
-        firstChild.simulate("click", {})
+        firstChild.simulate( "click", {} )
 
         //expect first child is still first child 
-        expect(wrapper.find('.prompts').childAt(0)).to.have.value(props.prompts[0])
+        expect( wrapper.find( '.prompts' ).childAt( 0 ) ).to.have.value( props.prompts[0] )
         //expect second child third prompt
-        expect(wrapper.find('.prompts').childAt(1)).to.have.value(props.prompts[2])
-    })
-    it('handles click on second prompt and re-renders with second prompt in place of first, and new prompt in place of second',  () => {
-        const wrapper = shallow(<Bracket {...props}/>)
-        const prompts = wrapper.find('.prompts')
+        expect( wrapper.find( '.prompts' ).childAt( 1 ) ).to.have.value( props.prompts[2] )
+    } )
+    it( 'handles click on second prompt and re-renders with second prompt in place of first, and new prompt in place of second',  () => {
+        const wrapper = shallow( <Bracket {...props}/> )
+        const prompts = wrapper.find( '.prompts' )
         
-        const firstChild = prompts.childAt(0)
-        const secondChild = prompts.childAt(1)
+        const firstChild = prompts.childAt( 0 )
+        const secondChild = prompts.childAt( 1 )
 
         //check initial values of children
-        expect(firstChild).to.have.value(props.prompts[0])
-        expect(secondChild).to.have.value(props.prompts[1])
+        expect( firstChild ).to.have.value( props.prompts[0] )
+        expect( secondChild ).to.have.value( props.prompts[1] )
         
-        secondChild.simulate("click", {})
+        secondChild.simulate( "click", {} )
 
         //expect first child is prior second child
-        expect(wrapper.find('.prompts').childAt(0)).to.have.value(props.prompts[1])
+        expect( wrapper.find( '.prompts' ).childAt( 0 ) ).to.have.value( props.prompts[1] )
         //expect second child is third prompt
-        expect(wrapper.find('.prompts').childAt(1)).to.have.value(props.prompts[2])
-    })
-})
+        expect( wrapper.find( '.prompts' ).childAt( 1 ) ).to.have.value( props.prompts[2] )
+    } )
+} )
