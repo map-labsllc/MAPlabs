836d3d76dcc890fcad1769cd7223e787b2acf28b
John Maddock <jmmaddock@gmail.com>
2019-01-31 15:17:04 -0700

    starting dash

---
+++ src/Components/Framework/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../CSS/Dashboard.css'
+import { Col, Row, Container, Panel } from 'react-bootstrap'
+const Dashboard = () => {
+  return (
+    <div className="background">
+      <Container>
+        <Row>
+          <Col>
+
+          </Col>
+          <Col>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container >
+    </div >
+  )
+}
+
+export default Dashboard
