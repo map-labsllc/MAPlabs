5ab601feed1962a2dd517e2748b39555bebf0c43
Ed Phelps <edphelps1@gmail.com>
2019-01-30 21:05:00 -0700

    intial commit

---
+++ src/Components/Modules/Module3Text.js
@@ -0,0 +1,81 @@
+export const MOD_3_DESC = `Welcome to Module 3, in which you will explore your deep, authentic desires.
+<br><br>
+Desire plays an important role in well-being. In Module 3 we will focus on differentiating between inauthentic desires, which we appropriate from the influences and distractions around us, and our authentic ones, which we source from our deepest selves.
+<br><br>
+In Module 1 we differentiated between our current situation, which may have resulted from extrinsic motivations, and our desired future life, which can be built on intrinsic motivations.
+<br><br>
+In Module 2, a primary takeaway was that often our inhibiting values and beliefs make their way into our lives through the relational, social, and cultural influences around us. These “adopted” values and beliefs can make us feel like we desire things (or should, at any rate) that actually lead to our dissatisfying current situation. Often this is because these extrinsic desires are not coming from our authentic, intrinsic values and beliefs.
+<br><br>
+In Module 3 we will continue our process by building on the intrinsic values and beliefs that we identified in Module 2 to help lead us to the deepest desires we have for our lives across a battery of life elements.`
+
+
+export const QUES_310_DESC = `In this exercise you will articulate your deepest desires in life. We have given you 7 different categories of life to guide your reflection (plus room for “other”). The categories are:
+<br><br>
+- financial/material
+<br><br>
+- vocation/career/life work
+<br><br>
+- social/community
+<br><br>
+- family
+<br><br>
+- mental/education
+<br><br>
+- spiritual/emotional/creative
+<br><br>
+- physical/health/recreation
+<br><br>
+- other
+<br><br>
+Think of them as big containers. Together, they provide space for a wide range of possible desires, and anything you come up with can likely fit into them. In fact, we want you to articulate all the things you desire—no matter how trivial, selfish, far-fetched, or embarrassing they may seem.
+<br><br>
+The goal of this exercise is for you to be as honest as you can be about what you feel most in your heart of hearts. It is okay if you find that self-honesty does not just come gushing out initially. Often we need to work through our more “rational” mindset at first before we can dig down to what our emotional self wants.
+<br><br>
+So feel free to just put down every thought that comes to mind about what you desire from each category of your life. Ultimately you will eliminate the ones that are not very important to you. This is essentially a big data dump; no one else will ever see what you put down unless you want them to. Don't worry about whether your wants and desires make sense,
+Some categories may prove much more fertile than others, but you should strive to include at least 10 - 20 listings (and many more if appropriate) in each category. At this stage, the point is not to reach conclusions, but to create a list of all the desires and wants, even the little ones, that you have for each category of your life.
+<br><br>
+<b>Brainstorm and list your desires in each category.</b> In each of the 7 categories listed, insert words or short phrases that describe specific desires you have for each category.  Please be as specific as you can and do not combine multiple desires into any one entry (for instance, instead of listing 'connecting to and benefiting other people,' create 2 entries, one for 'connecting to other people' and one for 'benefiting other people').`
+
+
+export const QUES_320_DESC = `Listing your desires is an important first step, one that can be hard. But there is more to do here. The next step is to go through a process of filtering that will allow you to decide which desires are most important to you within each category.
+<br><br>
+Within each category, compare the desires you listed, and make a tradeoff between each set of two, choosing only one of the two desires to carry into the next round.  For example, if you listed ten desires in one category, then compare your first listed desire to your second listed desire, and choose only one of them.  Then do the same for desires three and four, etc… At the end of round 1 you will have culled your desires in this category from ten to five.  Then do the same in the next round, and so on, until you only have one primary desire within the category.  Do this exercise for all seven categories until you only have one primary desire for each category.
+<br><br>
+This exercise has two main aims.
+<br><br>
+First, and most basically, it allows you to identify and focus on your deepest, most compelling desires, while letting other secondary desires fall to the sidelines. That will be valuable for developing your sense of purpose, since purpose is inherently based on what is most meaningful to you. Seeing what you choose as your most compelling desires, as well as seeing what you don’t choose, can help you to know what you are most striving for among a field of possible options.
+<br><br>
+Second, and more deeply, this exercise will give you an opportunity to be visceral rather than logical or reasonable, to sit in your body and feel your way through what is most important to you. Purpose is not something you reason your way into. The logic you use may very well stem from the unacknowledged expectations of others. Purpose takes root when you find yourself committed to something almost despite the better reasons one could find for taking another path. If the path makes sense to you, it has to make sense on the level of the heart, not just the head. As the philosopher Blaise Pascal wrote, “The heart has reasons which the reason knows not.”
+<br><br>
+<i>A caveat:<i> when you get down to your last few desires, you may find that they are equally important, and you may want to combine some of them to come up with an optimal final desire. There will be an opportunity for you to make this kind of revision in the last two rounds.`
+
+
+export const QUES_330_DESC = `Now that you have distilled your desires to what is most important to you within each of the 7 categories, it is now time to compare and prioritize between the categories themselves. The process here works the same way as the previous tradeoffs, but here we get to compare parts of our lives that are distinct and decide what role and proportion they each have in our lives. Again, you will make tradeoffs between the final desires of the 7 categories until only one desire remains. As before, there will be an opportunity for you to revise your last few desires in the final two rounds.`
+
+
+export const QUES_340_DESC = `When you are finished, you will be able to see all of your final desires for each category in one place. Think about them for a few minutes. Taken together, what do they suggest about the life that you desire? Also think about how you prioritized each category in your second tradeoff exercise. In the space provided, write a short statement that brings together all the desires from each category and captures the essence of what this exercise has revealed to you, using the phrases themselves. As you organize the statement, pay attention to how you prioritized among these desires.`
+
+
+export const QUES_350_DESC = `Review the 'desires statement' you created in Exercise 4 and consider how well the statement captures your most authentic self.
+<br><br>
+List any important <i>themes<i> that you noticed across your 7 categories and in your ultimate choices about which of your desires are most important.
+<br><br>
+Which desires provide you with the most personal senses of the <i>meaning</i> and 'intrinsic' motivation in your life?
+<br><br>
+Name some <i>things beyond yourself</> that you could serve if you lived more authentically  from your deepest and truest desires.
+<br><br>
+List the areas of <i>personal growth</i> that will enable your life to be more expressive of your desires.
+<br><br>
+List which <i>relationships</i> that you either currently have or need to develop in the future (to any influence, such as people, groups, practices, experiences, etc.) are most important to supporting your life being lived our of your most authentic desires.
+<br><br>
+List any areas of <i>engagement</i> or <i>mastery</i> (either in your life’s work or avocationally) that could express or fulfill your deepest desires.`
+
+
+export const QUES_360_DESC = `In order to create more personal Autonomy, consider which desires are less authentic and need to be “broken from,” and which desires are more authentic and need to be “built toward.” Fill in the following blanks:
+<br><br>
+In order to create a more meaningful life, I want to increase my personal Autonomy by altering my: `
+
+
+export const QUES_370_DESC = `In order to create more personal Autonomy, consider which desires are less authentic and need to be “broken from,” and which desires are more authentic and need to be “built toward.” Fill in the following blanks:
+<br><br>
+In order to create a more meaningful life, I want to increase my personal Autonomy by altering my: `
