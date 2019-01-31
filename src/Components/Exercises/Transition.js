24c0ef9269930e36056b4d947847f0e98104b7c4
John Maddock <jmmaddock@gmail.com>
2019-01-31 11:46:33 -0700

    style adjust

--- src/Components/Exercises/Transition.js
+++ src/Components/Exercises/Transition.js
@@ -7,6 +7,7 @@ import {
   Form,
   FormControl,
   FormGroup,
+  Glyphicon
 } from 'react-bootstrap'
 
 /* **************************************************
@@ -35,59 +36,59 @@ export default class Transition extends React.Component {
 
   // **************************************************
   // set isDirty and update state with new transition.from
-  onChangeFrom = ( e ) => {
+  onChangeFrom = (e) => {
     // console.log("Transition::onChangeFrom(), e: ", e.target.value);
 
     const newTransition = { ...this.state.transition }
     newTransition.from = e.target.value
 
-    this.setState( {
+    this.setState({
       isDirty: true,
       transition: newTransition,
-    } )
+    })
   }
 
   // **************************************************
   // set isDirty and update state with new transition.to
-  onChangeTo = ( e ) => {
+  onChangeTo = (e) => {
     // console.log("Transition::onChangeTo(), e: ", e.target.value);
 
-    const newTransition = {...this.state.transition}
+    const newTransition = { ...this.state.transition }
     newTransition.to = e.target.value
 
-    this.setState( {
+    this.setState({
       isDirty: true,
       transition: newTransition,
-    } )
+    })
   }
 
 
   // **************************************************
   // pass to parent to update value and clear isDirty
-  onBlur = ( e ) => {
-    console.log( "Transition::onBlur(), e: ", e.target.value )
+  onBlur = (e) => {
+    console.log("Transition::onBlur(), e: ", e.target.value)
     const { updateTransitionCB, id } = this.props
     const { isDirty, transition } = this.state
-    if ( isDirty ) {
-      updateTransitionCB( id, transition )
-      this.setState( {
+    if (isDirty) {
+      updateTransitionCB(id, transition)
+      this.setState({
         isDirty: false,
-      } )
+      })
     }
   }
 
   // **************************************************
   // pass to parent to delete
   onclickDelete = () => {
-    console.log( "Transition::onclickDelete()" )
+    console.log("Transition::onclickDelete()")
     const { deleteTransitionCB, id } = this.props
-    deleteTransitionCB( id )
+    deleteTransitionCB(id)
   }
 
   // **************************************************
   // render!
   render() {
-    console.log( "Transition::render()" )
+    console.log("Transition::render()")
 
     // initialize
     let { transition, isDirty } = this.state
@@ -101,25 +102,26 @@ export default class Transition extends React.Component {
 
     return (
       <Form inline onSubmit={this.onSubmit}>
-        <FormGroup>
-          <Button type = "button" onClick={this.onclickDelete}>del</Button>
-          {' '}
-          <FormControl
-            type = "text"
-            onChange = {this.onChangeFrom}
-            onBlur = {this.onBlur}
-            value = {transition.from}
-            placeholder = "Please enter a from"
-          />
-          <FormControl
-            type = "text"
-            onChange = {this.onChangeTo}
-            onBlur = {this.onBlur}
-            value = {transition.to}
-            placeholder = "Please enter a to"
-          />
-          {` `}{id}{` `}{( isDirty ? 'dirty' : '' )}
-        </FormGroup>
+        <div className="text-center">
+          <FormGroup>
+            {' '}
+            <FormControl
+              type="text"
+              onChange={this.onChangeFrom}
+              onBlur={this.onBlur}
+              value={transition.from}
+              placeholder="Please enter a from"
+            />
+            <FormControl
+              type="text"
+              onChange={this.onChangeTo}
+              onBlur={this.onBlur}
+              value={transition.to}
+              placeholder="Please enter a to"
+            />
+          </FormGroup>
+          <Button type="button" onClick={this.onclickDelete}><Glyphicon glyph="trash"></Glyphicon></Button>
+        </div>
       </Form>
     )
   }
