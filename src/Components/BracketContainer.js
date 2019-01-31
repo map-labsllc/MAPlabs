37c656b81b58c9133edbcb5eb7f4a66133836442
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-31 11:22:13 -0700

    exercise 330

--- src/Components/BracketContainer.js
+++ src/Components/BracketContainer.js
@@ -3,9 +3,11 @@ import Bracket from './Bracket'
 import { connect } from 'react-redux'
 import { persistAnswersAC, updateAnswersAC } from '../store/answers/actions'
 
+const prompts330 = [ "Financial/Material", "Vocation/Career/Life Work", "Social/Community", "Family", "Mental/Educational", "Spiritual/Emotional/Creative", "Physical/Health/Recreational", "Other"]
 function mapStateToProps( state, { question, isDynamic } ) {
     return {
-        prompts: state.answersRD.questions[question.promptCode],
+        prompts: ( isQuestion330( state, question ) ) && ( question.promptCode == 330 ) ? prompts330
+        : state.answersRD.questions[question.promptCode],
         userId: state.userRD.user.user_id,
         question,
         isDynamic
@@ -21,6 +23,9 @@ function mapDispatchToProps( dispatch ) {
     } 
 }
 
+function isQuestion330( state, question ) {
+    return state.answersRD.questions[question.promptCode] && !state.answersRD.questions[question.promptCode][0]
+}
 export default connect(
     mapStateToProps,
     mapDispatchToProps
