70f0c5fc7853159911be6b748fe9046110457d54
John Maddock <jmmaddock@gmail.com>
2019-02-01 10:21:18 -0700

    dashboard style

--- src/Components/Framework/Dashboard.js
+++ src/Components/Framework/Dashboard.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import { Col, Row, Panel, PanelGroup } from 'react-bootstrap'
-
+import { Col, Row, Panel, PanelGroup, Grid } from 'react-bootstrap'
+import '../../CSS/Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div style={style.background}>
-
-      <div className="container-fluid">
-        <Row style={style.bigContain}>
-          <div className="text-center">
+    <div style={style.backgroundDash} className="container-fluid">
+      <Grid className="bigContain">
+        <Row >
+          <div className="text-center" >
             <h1>Dashboard</h1>
           </div>
-          <div style={style.lineUp}>
+          <div className="lineup">
             <Col md={4} lg={4}>
               <h5>Current Situation M5 **Exercise 1**</h5>
-              <div className="container-fluid" style={style.smallContain}><p>Currently sitting in galvanize</p></div>
+              <div className="container-fluid smallContain"><p>Currently sitting in galvanize</p></div>
               <h5>Future Desired Situation M5 **Exercise 1**</h5>
-              <div className="container-fluid" style={style.smallContain}><p>THE FUTURE</p></div>
+              <div className="container-fluid smallContain" ><p>THE FUTURE</p></div>
               <h5>Key Themes on Self-Knowlege **M5 Exercise 1**</h5>
-              <div className="container-fluid" style={style.smallContain}><p>THEMES</p></div>
+              <div className="container-fluid smallContain" ><p>THEMES</p></div>
               <h5>Summarizing Thoughts or Directives **M5 Excercise 1**</h5>
-              <div className="container-fluid" style={style.smallContain}><p>MY THOUGHTS ON THE DIRECTIVES ETC</p></div>
+              <div className="container-fluid smallContain"><p>MY THOUGHTS ON THE DIRECTIVES ETC</p></div>
             </Col>
             <Col md={4} lg={4}>
               <PanelGroup accordian bsStyle="primary">
@@ -68,46 +67,46 @@ const Dashboard = () => {
             </Col>
             <Col md={4} lg={4}>
               <h5>Key Themes on the Beyond the Self service *****M5 Exercise 3****</h5>
-              <div className="container-fluid" style={style.smallContain}><p>THEMS EKSDLJFJGILJGLSDIJGLKDJGLSIDJFGIJSDFIJ</p></div>
+              <div className="container-fluid smallContain">
+                <p>THEMS EKSDLJFJGILJGLSDIJGLKDJGLSIDJFGIJSDFIJ</p>
+              </div>
               <h5>Summarizing Thoughts or Directives ****M5 Exercise 3 *****</h5>
-              <div className="container-fluid" style={style.smallContain}><p>answers go here I believe</p></div>
+              <div className="container-fluid smallContain">
+                <p>answers go here I believe</p>
+              </div>
+              <h5>From the elements you chose, this is your purpose statement ***M5 E4***</h5>
+              <div className="container-fluid smallContain">
+                <p>My purpose is this</p>
+              </div>
+              <div>Key commitments about personal growth ****M5 E5***</div>
+              <div className="container-fluid smallContain">
+                <p>These are my commitments to personal growth</p>
+              </div>
+              <h5>Key Relationships ****M5 E5***</h5>
+              <div className="container-fluid smallContain">
+                <p>These are important relations</p>
+              </div>
+              <h5>Key themes on engagement mastery ***M5 E5***</h5>
+              <div className="container-fluid smallContain">
+                <p>Engagementssssssss</p>
+              </div>
+              <h5>Break/Build Commitments</h5>
+              <div className="container-fluid smallContain">
+                <p>Break these and Build these commitments etc etccc</p>
+              </div>
             </Col>
           </div>
         </Row>
-      </div>
+      </Grid>
     </div >
   )
 }
 
 const style = {
-  background: {
+  backgroundDash: {
     backgroundImage: "url('trees.jpg')",
     backgroundSize: "cover",
   },
-  bigContain: {
-    backgroundColor: "white",
-    paddingRight: "2%",
-    paddingLeft: "2%",
-    paddingTop: "2%",
-    paddingBottom: "2%",
-    marginTop: "6%",
-    marginRight: "3%",
-    marginLeft: "3%",
-    marginBottom: "4%",
-    borderRadius: "10px",
-    boxShadow: "5px 5px 50px 10px grey",
-  },
-  lineUp: {
-    display: "inline-block",
-  },
-  smallContain: {
-    color: "black",
-    backgroundColor: "#59C3F8",
-    opacity: "0.3",
-    borderRadius: "25px",
-
-  }
-
 }
 
 export default Dashboard
