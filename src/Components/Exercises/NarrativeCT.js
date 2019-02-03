5fccd3edb609e12db3e533bb2d67e3856ce72e14
Ed Phelps <edphelps1@gmail.com>
2019-02-03 08:41:20 -0700

    updated comments

--- src/Components/Exercises/NarrativeCT.js
+++ src/Components/Exercises/NarrativeCT.js
@@ -13,7 +13,8 @@ import {
     promptQuestionCode -- integer -- Object with 0 or questionCode to
       use as prompts for this Narrative.  questionCode must be a quesiton in answerRD.
     question -- { code: 50, text: "question 50" }
-    instructions -- string with instructions, can be an empty string for no instructions
+    description
+    instructions 
     isDynamic -- undefined or true
                  rendering static version in Popup or dynamic verison in Modal
     onCloseModalCB -- call to close the modal
