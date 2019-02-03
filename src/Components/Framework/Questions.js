4f74f416198bf8d846910ccfffeb5b9535e9617a
Ed Phelps <edphelps1@gmail.com>
2019-02-02 20:51:18 -0700

    removed comment

--- src/Components/Framework/Questions.js
+++ src/Components/Framework/Questions.js
@@ -115,12 +115,6 @@ export default class Questions extends React.Component {
     // ******************************************
     // render dynamic verison in <ModalX>
 
-    // TODO:  Yikes, make this more efficient!  Figure out which one will be
-    //        rendered and just do cloneElement() on that one and render it.
-    //        No need to create a new array of all of them.
-    //        Or maybe this won't work because of the key={idx} or maybe
-    //        we don't even need that if we're only rendering a single component.
-
     // inject isDynamic into props so the subCompoent will render its dynamic version
     const subComponentsWithIsDynamic = subComponents.map( ( subComponent ) => {
       return React.cloneElement(
@@ -130,7 +124,6 @@ export default class Questions extends React.Component {
         }
       )
     } )
-
 
     // NOTE: The <div key = {idx}> tag is used to suppress React warning about
     //       elements needing a unique key.
