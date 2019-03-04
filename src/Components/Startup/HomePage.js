4c46b5d7cf927a5547932b87f27b66d7538082f8
Christa Sparks <christa@Christas-MacBook-Pro.local>
2019-03-04 09:12:48 -0700

    home page redesigned

--- src/Components/Startup/HomePage.js
+++ src/Components/Startup/HomePage.js
@@ -1,58 +1,94 @@
 import React from "react"
-import { Navbar, Nav, NavItem, Jumbotron, Button } from "react-bootstrap"
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  Jumbotron,
+  Button,
+  Row,
+  Col
+} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 // import {Brand,} from "react-bootstrap/lib/Nav"
 
-import '../../CSS/HomePage.css'
+import "../../CSS/HomePage.css"
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div style={style.container} className="container">
+      <div style={style.container} className="pageContainer">
+        {/* <div className="container"> */}
         <Navbar
           style={style.navbar}
           className="navbar"
           bg="light"
           variant="light"
         >
-          <Navbar.Brand style={style.navlink} className="navlink" href="/">
+          <Navbar.Brand style={style.homelink} className="homelink" href="/">
             MAPMaker
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav
-            variant="pills"
-            className="mr-auto navlinks"
-          >
-            <NavItem>
-              <NavLink style={style.signup} className="signup" to="/signup">
-                Signup
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={style.login} className="login" to="/login">
-                Login
-              </NavLink>
-            </NavItem>
-          </Nav>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav variant="pills" className="mr-auto navlinks">
+              <NavItem>
+                <NavLink style={style.signup} className="signup" to="/signup">
+                  Signup
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={style.login} className="login" to="/login">
+                  Login
+                </NavLink>
+              </NavItem>
+            </Nav>
           </Navbar.Collapse>
         </Navbar>
         <Jumbotron style={style.jumboimage}>
           <div className="subheader">
-            <p>The science of meaning and purpose.</p>
+            <p className="subText">The science of meaning and purpose.</p>
           </div>
           <div className="headContainer">
             <h1 className="header">
               Discover how to inform, empower, and fulfill your life.
-              <Button href="https://www.map-labs.com/map-maker-core-meaning-and-purpose-program.html" target="_blank" className="infoBtn" variant="light">
-              Learn more about MAPMaker
-              <img style={{ width: "20px", paddingLeft: "5px" }} src="Arrow-Right.svg" alt="info icon"/>
+              <Button
+                href="https://www.map-labs.com/map-maker-core-meaning-and-purpose-program.html"
+                target="_blank"
+                className="infoBtn"
+                variant="light"
+              >
+                Learn more about MAPMaker
+                <img
+                  style={{ width: "20px", paddingLeft: "5px" }}
+                  src="Arrow-Right.svg"
+                  alt="info icon"
+                />
               </Button>
             </h1>
           </div>
           {/* <h3 className="signupHead" >Reveal your purpose.</h3> */}
+          <div className="signupHead">
+            {/* <Row className="signupText" >
+              <Col>
+                <h3>Reveal your purpose.</h3>
+              </Col>
+            </Row> */}
+            <Row className="signupBtn" >
+              <Col>
+              <h3><Button className="outline-secondary">Get Started</Button></h3> 
+              </Col>
+            </Row>
+            <Row className="loginText" >
+              <Col>
+                <h4 className="acct">
+                  Already have an account?{" "}
+                  <a className="loginLink">Login here.</a>
+                </h4>
+              </Col>
+            </Row>
+          </div>
           <div style={style.coloroverlay} className="coloroverlay" />
         </Jumbotron>
+        {/* </div> */}
       </div>
     )
   }
@@ -68,35 +104,39 @@ const style = {
     zIndex: 1,
     height: "100%",
     width: "100%",
-    border: "none",
+    border: "none"
   },
-  container: {
+  pageContainer: {
     position: "relative",
     height: "100%",
     width: "100%",
     marginRight: "0px",
     marginLeft: "0px",
     paddingLeft: "0px",
-    paddingRight: "0px",
-  },
-  navlink: {
-    color: "white"
+    paddingRight: "0px"
   },
   login: {
     color: "white"
+  },
+  homelink: {
+    color: "white",
+    textDecoration: "underline",
+    fontSize: "32px",
+    fontWeight: "400",
+    fontFamily: "'Roboto', sans-serif"
   },
   jumboimage: {
     color: "white",
     backgroundImage: 'url("leaves.jpg")',
     backgroundSize: "cover",
     width: "100%",
-    height: "700px",
+    height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexWrap: "wrap",
     position: "relative",
-    marginBottom: "0px",
+    marginBottom: "0px"
   },
   coloroverlay: {
     width: "100%",
