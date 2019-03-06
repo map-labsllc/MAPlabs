70f8f6d014d353cab30784fabcbdce56deb28af5
Ed Phelps <edphelps1@gmail.com>
2019-03-06 15:23:25 -0700

    Initial StrengthXCT and StrengthX components
    Demoing them in Module 3

---
+++ src/Components/Exercises/StrengthX.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Button,
+} from 'react-bootstrap'
+import Transition from './Transition'
+import '../../CSS/ModalNavButtons.css'
+
+// legal values for an effect
+export const EFFECT_IMPEDIMENT = 'impediment'
+export const EFFECT_EMBODIMENT = 'embodiment'
+
+/* **************************************************
+   Strength component
+
+   Displays a single question with:
+     -- Selection list of strengths (ex: 'honesty')
+     -- Text control for thinking 'broadly' about strength
+     -- <Phrase> for each reflection (a phrase and its effect)
+     -- TODO: OR PUT THIS IN <PHRASES>:  Add button to add a new reflection (phrase/effect)
+
+   state:
+     Manage the strength, the 'broadly' text, and list of reflections (phrase/effect)
+     TODO: OR PUT THE LIST OF REFLECTIONS IN <PHRASES>
+
+   props:
+     number -- number of the question in <Questions> when isDynamic, 1-based
+     question -- { code: 50, text: "Question 50" }
+     previousData --  {
+                        strength: "honest",
+                        broadly: "broad thoughts",
+                        reflections: [ { reflection: "str", effect: "impediment/embodiment" }, {...} ]
+                      }
+     isDynamic -- undefined - render static version in <PopUp>
+                  true - render dynamic version <ModalX>
+     onUpdateStoreCB() -- callback to update the store
+***************************************************** */
+export default class StrengthX extends React.Component {
+
+  // -------------------------------------------------------
+  // UUID to be used as the component key
+  uuid = 1;
+
+  // get new UUID
+  getUUID = () => {
+    console.log("Strength::getUUID() returning ", this.uuid + 1)
+    return this.uuid++
+  }
+
+  // make a new reflectionWithKey
+  getNewReflectionWithKey = (reflection) => ({
+    key: this.getUUID(),
+    reflection,
+  })
+
+  // strip keys
+  stripKeys = (reflectionsWithKeys) => {
+    return reflectionsWithKeys.map(reflectionWithKey => reflectionWithKey.reflection)
+  }
+
+  // add keys
+  // addKeys = (transitions) => transitions.map(transition => ({ key: this.getUUID() , transition}))
+  addKeys = (reflections) => reflections.map((reflection) => {
+    console.log("addKeys: ", reflection)
+    return { key: this.getUUID(), reflection }
+  })
+  // -------------------------------------------------------
+
+  state = {
+    isDirty: false,
+    strength: this.props.previousData.strength,
+    broadly: this.props.previousData.broadly,
+    reflectionsWithKeys: this.addKeys(this.props.previousData.reflections)
+  }
+
+  // **********************************************
+  componentDidMount = () => {
+    // add an initial blank entry if there are no previous entries
+    // const { previousData } = this.props
+    // TODO: NEED TO ADD THIS TO <Phrases>:  if (previousData.reflections.length === 0) this.onclickAdd()
+  }
+
+  // **********************************************
+  // tell parent to update data to store
+  updateData = () => {
+    console.log(`Strength::updateData()`)
+
+    const { onUpdateStoreCB } = this.props
+
+    const newData = {}
+    newData.strength    = this.state.strength
+    newData.broadly     = this.state.broadly
+    newData.reflections = this.stripKeys(this.state.reflectionsWithKeys)
+
+    onUpdateStoreCB(newData)
+  }
+
+  // **********************************************
+  // render!
+  render() {
+    console.log("Strength::render()")
+
+    const { number, question, isDynamic } = this.props
+    const { reflectionsWithKeys } = this.state
+
+    console.log("reflectionsWithKeys", reflectionsWithKeys)
+    console.log("this.props.previousData", this.props.previousData)
+
+    if ( !isDynamic ) {
+
+      return (
+        <>
+          <p>STATIC data, should call Phrases components so they can render</p>
+          <p>this.state:</p>
+          <p>- strength: {this.state.strength}</p>
+          <p>- broadly: {this.state.broadly}</p>
+          <p>- reflections: </p>
+          <p>{JSON.stringify(this.state.reflectionsWithKeys)}</p>
+        </>
+      )
+    }
+
+    return (
+      <>
+        <p>{number}. DYNAMIC data</p>
+        <Button onClick={this.updateData}>update store</Button>
+        <p>this.state:</p>
+        <p>- strength: {this.state.strength}</p>
+        <p>- broad thought: {this.state.broadly}</p>
+        <p>- reflections: </p>
+        <p>{JSON.stringify(this.state.reflectionsWithKeys)}</p>
+      </>
+    )
+  }
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+StrengthX.propTypes = {
+  question: PropTypes.shape( {
+    code: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+  } ).isRequired,
+  previousData: PropTypes.object.isRequired,
+  isDynamic: PropTypes.bool,
+  onUpdateAnswerCB: PropTypes.func,  // required, injected by <Popup>
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
---
+++ src/Components/Exercises/StrengthXCT.js
@@ -0,0 +1,155 @@
+import { connect } from 'react-redux'
+import StrengthX from './StrengthX'
+import { getAnswers } from '../../store/answers/reducer'
+import { updateAnswersAC } from '../../store/answers/actions'
+
+// legal values for the IDX_EFFECT field of the
+export const EFFECT_BROADLY = 'broadly'
+
+
+// indexes into the data structure coming from the store
+const IDX_STRENGTH = 0
+const IDX_PHRASE = 1
+const IDX_EFFECT = 2
+
+/* *****************************************
+   mapStateToProps()
+
+   passedProps:
+     number -- number of the question in <Questions>, 1-based
+     question -- { code: 50, text: "question 50" }
+     isDynamic -- undefined - rendering static version in <Popup>
+                  true - rendering dynamic verison in <ModalX>
+******************************************** */
+const mapStateToProps = ( state, passedProps ) => {
+  console.log( "StrengthCT::mapStateToProps()" )
+
+  const {
+    number,
+    question,
+    isDynamic,
+  } = passedProps
+
+  // validate params
+  if ( !question || !question.code ) throw new Error( "missing question code: ", passedProps.question_code )
+
+  // get previous data, if any
+  const answerRecords = getAnswers( state.answersRD, question.code )
+  console.log( `getAnswers(${question.code}): `, answerRecords )
+
+  // data structure to pass down in props
+  const previousData = {
+    strength: "",
+    broadly: "",
+    reflections: [],  // array of objects in form { reflection: "str", effect: "impediment/embodiment" }
+    }
+
+  // translate data from the 2D array of strings to
+  //   the object structure to pass down as prop to <Strength>
+  if (answerRecords.length) {
+
+    // STRENGTH
+    // --------
+    previousData.strength = answerRecords[0][IDX_STRENGTH]
+
+    // check that each record has the same strength
+    if (!answerRecords.every(record => record[0] === previousData.strength)) {
+      console.log("ERROR, question.code:", question.code, "records should all have the same strength")
+    }
+
+
+    // BROADLY (the broad statement)
+    // -----------------------------
+    // check that first record is the 'broadly' type
+    if (!answerRecords[0][IDX_EFFECT] !== EFFECT_BROADLY) {
+      console.log("ERROR, first record is not 'broadly' for question.code", question.code)
+    }
+
+    previousData.broadly = answerRecords[0][IDX_PHRASE]
+
+
+    // REFLECTIONS array
+    // ----------------
+
+    const reflections = []
+    for (let i = 1; i < answerRecords.length; i++) {
+      reflections.push({
+        reflection: answerRecords[i][IDX_PHRASE],
+        effect:     answerRecords[i][IDX_EFFECT]
+      })
+    }
+    previousData.reflections = reflections
+  }
+
+
+  console.log('StrengthsCT::previousData: ', previousData)
+
+  return {
+    number,
+    question,
+    previousData,
+    isDynamic,
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/* *****************************************
+   mapDispatchToProps()
+
+   passedProps -- see mapStateToProps above
+******************************************** */
+const mapDispatchToProps = ( dispatch, passedProps ) => {
+
+  /* *****************************************
+     onUpdateStore()
+
+     Save the new transitions to store.  Does NOT persist.
+
+     newTransitions -- array of transitinos
+  ******************************************** */
+  function onUpdateStore( newData ) {
+    console.log( `StrengthCT::onUpdate(${newData})` )
+
+    const { question } = passedProps
+
+    // store wants 2D array of strings, so map newData into that format
+    const twoDimArrayOfString = []
+
+    if (newData.strength) {
+      twoDimArrayOfString[0] = []
+      twoDimArrayOfString[0][IDX_STRENGTH] = newData.strength
+      twoDimArrayOfString[0][IDX_PHRASE] = newData.broadly
+      twoDimArrayOfString[0][IDX_EFFECT] = EFFECT_BROADLY
+
+      newData.reflections.forEach((reflection) => {
+        const newRecord = []
+        newRecord[IDX_STRENGTH] = newData.strength
+        newRecord[IDX_PHRASE] = reflection.reflection
+        newRecord[IDX_EFFECT] = reflection.effect
+        twoDimArrayOfString.push(newRecord)
+      })
+    }
+
+    console.log("Will update store with: ")
+    console.log(twoDimArrayOfString)
+
+    // update store
+    dispatch( updateAnswersAC( question.code, twoDimArrayOfString ) )
+  }
+
+  /* *****************************************
+     The props being passed down
+  ******************************************** */
+  return {
+    onUpdateStoreCB: onUpdateStore,
+  }
+
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)( StrengthX )
