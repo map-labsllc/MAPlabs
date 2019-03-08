7600c56488563c28cc05a1061a245f2f02ba87b0
Christa Sparks <christa@Christas-MacBook-Pro.local>
2019-03-08 15:44:24 -0700

    barebones form created

--- src/Components/Exercises/StrengthX.js
+++ src/Components/Exercises/StrengthX.js
@@ -1,14 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {
-  Button,
-} from 'react-bootstrap'
-import '../../CSS/ModalNavButtons.css'
-import { UUID } from '../Utils/UUID'
+import React from "react"
+import PropTypes from "prop-types"
+// import { Button } from "react-bootstrap"
+import { Form, Dropdown, TextArea, Select, Button } from "semantic-ui-react"
+import "../../CSS/ModalNavButtons.css"
+import { UUID } from "../Utils/UUID"
 
 // legal values for an effect
-export const EFFECT_IMPEDIMENT = 'impediment'
-export const EFFECT_EMBODIMENT = 'embodiment'
+export const EFFECT_IMPEDIMENT = "impediment"
+export const EFFECT_EMBODIMENT = "embodiment"
+
+const options = [
+  {
+    key: "0",
+    text: "Appreciation of beauty and excellence",
+    value: "appreciation of beauty and excellence"
+  },
+  { key: "1", text: "Bravery", value: "bravery" },
+  { key: "2", text: "Creativity", value: "creativity" },
+  { key: "3", text: "Curiosity", value: "curiosity" },
+  { key: "4", text: "Fairness", value: "fairness" },
+  { key: "5", text: "Forgiveness", value: "forgiveness" },
+  { key: "6", text: "Gratitude", value: "gratitude" },
+  { key: "7", text: "Honesty", value: "honesty" },
+  { key: "8", text: "Hope", value: "hope" },
+  { key: "9", text: "Humility", value: "humility" },
+  { key: "10", text: "Humor", value: "humor" },
+  { key: "11", text: "Judgment", value: "judgment" },
+  { key: "12", text: "Kindness", value: "kindness" },
+  { key: "13", text: "Leadership", value: "leadership" },
+  { key: "14", text: "Love", value: "love" },
+  { key: "15", text: "Love of learning", value: "love of learning" },
+  { key: "16", text: "Perseverance", value: "perseverance" },
+  { key: "17", text: "Perspective", value: "perspective" },
+  { key: "18", text: "Prudence", value: "prudence" },
+  { key: "19", text: "Self-regulation", value: "self-regulation" },
+  { key: "20", text: "Social intelligence", value: "social intelligence" },
+  { key: "21", text: "Spirituality", value: "spirituality" },
+  { key: "22", text: "Teamwork", value: "teamwork" },
+  { key: "23", text: "Zest", value: "zest" }
+]
+
+const bodimentOpts = [
+  { key: "0", text: "Embodiment", value: "em" },
+  { key: "1", text: "Impediment", value: "im" }
+]
 
 /* **************************************************
    Strength component
@@ -36,14 +71,14 @@ export const EFFECT_EMBODIMENT = 'embodiment'
      onUpdateStoreCB() -- callback to update the store
 ***************************************************** */
 export default class StrengthX extends React.Component {
-
   uuid = new UUID() // provides unique keys for <StrengthX> components
 
   state = {
     isDirty: false,
     strength: this.props.previousData.strength,
     broadly: this.props.previousData.broadly,
-    reflectionsWithKeys: this.uuid.addKeys(this.props.previousData.reflections)
+    reflectionsWithKeys: this.uuid.addKeys(this.props.previousData.reflections),
+    phrases: [],
   }
 
   // **********************************************
@@ -61,26 +96,27 @@ export default class StrengthX extends React.Component {
     const { onUpdateStoreCB } = this.props
 
     const newData = {}
-    newData.strength    = this.state.strength
-    newData.broadly     = this.state.broadly
+    newData.strength = this.state.strength
+    newData.broadly = this.state.broadly
     newData.reflections = this.uuid.stripKeys(this.state.reflectionsWithKeys)
 
     onUpdateStoreCB(newData)
   }
 
+  handleChange = (e, { value }) => this.setState({ strength: value })
+
   // **********************************************
   // render!
   render() {
     console.log("Strength::render()")
-
+    const { strength, phrases } = this.state
     const { number, question, isDynamic } = this.props
     const { reflectionsWithKeys } = this.state
 
     console.log("reflectionsWithKeys", reflectionsWithKeys)
     console.log("this.props.previousData", this.props.previousData)
 
-    if ( !isDynamic ) {
-
+    if (!isDynamic) {
       return (
         <>
           <p>STATIC data, should call Phrases components so they can render</p>
@@ -93,16 +129,46 @@ export default class StrengthX extends React.Component {
       )
     }
 
+    let morePhrases = phrases.map(phrase => {
+      return (<div>
+      <Form.Input placeholder='Phrase' />
+      <Select options={bodimentOpts} placeholder="Embodiment or Impendiment" />
+      </div>)
+    })
+
     return (
       <>
-        <p>{number}. DYNAMIC  data</p>
-        <Button onClick={this.updateData}>update store</Button>
-        <p>this.state:</p>
-        <p>- strength: {this.state.strength}</p>
-        <p>- broad thought: {this.state.broadly}</p>
-        <p>- reflections: </p>
-        <p>{JSON.stringify(this.state.reflectionsWithKeys)}</p>
+        <Form onSubmit={this.onSubmit} inverted>
+          <Dropdown
+            search
+            selection
+            options={options}
+            placeholder="Select Strength"
+            onChange={this.handleChange}
+            value={strength}
+          />
+          <TextArea placeholder="Reflect broadly and generally on this strength." />
+          <Form.Group >
+            <Form.Input placeholder='Phrase' />
+            <Select options={bodimentOpts} placeholder="Embodiment or Impendiment" />
+            {morePhrases}
+          </Form.Group>
+          <Button 
+          onClick={this.onPhraseAddClick}
+          >
+          Add Phrase
+          </Button>
+        </Form>
       </>
+      // <>
+      //   <p>{number}. DYNAMIC  data</p>
+      //   <Button onClick={this.updateData}>update store</Button>
+      //   <p>this.state:</p>
+      //   <p>- strength: {this.state.strength}</p>
+      //   <p>- broad thought: {this.state.broadly}</p>
+      //   <p>- reflections: </p>
+      //   <p>{JSON.stringify(this.state.reflectionsWithKeys)}</p>
+      // </>
     )
   }
 }
@@ -112,13 +178,13 @@ export default class StrengthX extends React.Component {
 ////////////////////////////////////////////////////////////////
 
 StrengthX.propTypes = {
-  question: PropTypes.shape( {
+  question: PropTypes.shape({
     code: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-  } ).isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired,
   previousData: PropTypes.object.isRequired,
   isDynamic: PropTypes.bool,
-  onUpdateAnswerCB: PropTypes.func,  // required, injected by <Popup>
+  onUpdateAnswerCB: PropTypes.func // required, injected by <Popup>
 }
 
 ////////////////////////////////////////////////////////////////
