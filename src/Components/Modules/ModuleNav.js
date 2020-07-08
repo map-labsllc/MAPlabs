bf8f75e9102b93d6df51ff02f506ccf2a9772386
Craig Quincy <craig.quincy@techstars.com>
2020-07-08 08:21:42 -0600

    prettier module Nav

--- src/Components/Modules/ModuleNav.js
+++ src/Components/Modules/ModuleNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
+import { ProgressBar } from 'react-bootstrap'
 
 const ModuleNav = ({moduleId, sections}) => {
 
@@ -13,16 +14,46 @@ const ModuleNav = ({moduleId, sections}) => {
       <div className="card-header">
         <h3>Exercises</h3>
       </div>
-      <ul className="list-group list-group-flush">
-        {sections.map(section => (
-          <li className="list-group-item">
-            {createLink(moduleId, section.id, section.title)}
-          </li>
-          )
-        )}
-      </ul>
+      <div className="card-body ">
+        <div className="table-full-width">
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td className="text-left">
+                    <h2>0</h2>
+                  </td>
+                  <td className="text-left">
+                    <p>
+                      {createLink(moduleId, 'intro', 'Introduction')} 
+                    </p>
+                  </td>
+                  <td className="text-left">
+                    <ProgressBar className="sectionProgress" now={50} label={'50%'}/>
+                  </td>
+                </tr>
+                {sections.map(section => (
+                  <tr>
+                    <td className="text-left">
+                      <h2>{section.id}</h2>
+                    </td>
+                    <td className="text-left">
+                      <p>
+                        {createLink(moduleId, section.id, section.title)}
+                      </p>
+                    </td>
+                    <td className="text-left">
+                      <ProgressBar className="sectionProgress" now={50} label={'50%'}/>
+                    </td>
+                  </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
     </div>
   )
 }
+
 
 export default ModuleNav
