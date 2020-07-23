7d4de348c2174645cdb0f6d283bc9652ed20fa1b
Craig Quincy <craig.quincy@techstars.com>
2020-07-23 07:17:57 -0600

    style Tips

--- src/Components/Startup/InfoPage.js
+++ src/Components/Startup/InfoPage.js
@@ -7,6 +7,7 @@ import {
 import { NavLink } from 'react-router-dom'
 
 import { connect } from 'react-redux'
+import Tips from './Tips'
 
 const InfoPage = ({ user }) => {
 
@@ -58,111 +59,7 @@ const InfoPage = ({ user }) => {
           <p>Here are some tips for specific practices and mentalities that can help you to get the most out of MAPmaker:</p>
         </div>
 
-
-        {/* SEVEN TIPS */}
-        {/* ------------------------------ */}
-
-        { /*pass id prop to prevent react runtime warning*/ }
-        <ListGroup id="0" accordian="true" bsStyle="primary">
-
-          {/* TIP 1 */}
-          <Card eventKey="1">
-            <Card.Header>
-              <Card.Title>
-                1.	Commit to the process.
-              </Card.Title>
-            </Card.Header>
-            <Card.Text>
-              One element of the scientific definition of purpose is “commitment.” Even if you do not yet have a sense of purpose, you can commit to the process of searching for it. Practically speaking, this commitment will work best by scheduling time for MAPmaker into your calendar. Everyone’s schedule is different, so try to find the times in your days and weeks that can consistently allow you to engage with this important investment in your future.
-            </Card.Text>
-          </Card>
-
-          {/* TIP 2 */}
-          <Card eventKey="2">
-            <Card.Header>
-              <Card.Title toggle> 
-                2.	Promise to be honest with yourself.
-              </Card.Title>
-            </Card.Header>
-            <Card.Text>
-              <p>This is harder than it sounds.  Chances are that questions about your meaning and purpose have nagged you for a long time, and that you have gotten used to ignoring them and soldiering on with life.  Your default stance may be one of accepting the status quo and allowing your pre-established conceptions to hold root, despite your growing awareness (whether conscious or subconscious) that your old patterns, beliefs, and principles may no longer be sufficient to sustain your growing, evolving self.  But with more honesty, you will likely find new personal insights and understandings that you have consciously or unconsciously avoided.</p>
-              <p>Plus, we tend to internalize the voices of other people.  Even in the solitude of our own thoughts, they can quietly dictate what is admissible and what is not.  As you encounter yourself and the questions of meaning and purpose in the exercises, sense what your body is feeling.  Often we experience subtle, nonverbal, physical feelings that indicate the need for further awareness and investigation.  With heightened awareness and honesty we can often find personal truths that are counter to the truths that we have internalized and accepted from others.</p>
-              <p>Throughout these modules, you will see the most benefit if you allow yourself to think, speak, and write freely and often. The more time and effort you put into digging into your deeper self, the more revelatory and profound your results will be.  This is particularly true of the moments when your most authentic senses and voices want to challenge the accepted “truths” that you have held. Try to allow yourself to honestly encounter these challenges even if they mean debunking some of the old “truths” that have directed your life to date.</p>
-            </Card.Text>
-          </Card>
-
-          {/* TIP 3 */}
-          <Card eventKey="3">
-            <Card.Header>
-              <Card.Title> 3.	Open yourself to new ideas.</Card.Title>
-            </Card.Header>
-            <Card.Text>
-              <p>Because MAPmaker will expose you to ways of thinking about yourself that you may never have considered before, or that you have previously avoided, it is important to approach the program with a willingness to entertain both new ideas and any old ideas that you may have avoided, but that you now come to understand as informing who you are.</p>
-              <p>People who are open to new ideas and experiences have demonstrably higher levels of well-being than those who are not.  Adopting this posture will enhance your efforts of finding meaning and purpose, and it will serve the MAPPERS element of Personal growth.</p>
-            </Card.Text>
-          </Card>
-
-          {/* TIP 4 */}
-          <Card eventKey="4">
-            <Card.Header>
-              <Card.Title>
-                4. Embrace discomfort.
-              </Card.Title>
-            </Card.Header>
-            <Card.Text>
-              The exercises might stir up a thought or desire that seems risky, something that makes you feel uncomfortable.  Pay attention to that feeling.  Resist the instinct to make it go away.  Instead, take a step back and think about why you are uncomfortable and what the underlying trigger of the discomfort is.  Often such discomfort arises from a perceived conflict between your deepest sense of personal truth that is being provoked, and a constraint in your life that seems to be at odds with your truth.  Most likely, this personal truth is not going to go away, and resolving the conflict it produces may be a major key to developing more meaning and purpose in your life.
-            </Card.Text>
-          </Card>
-
-          {/* TIP 5 */}
-          <Card eventKey="5">
-            <Card.Header>
-              <Card.Title toggle> 
-                5.	Dare to not seek “happiness.”
-              </Card.Title>
-            </Card.Header>
-            <Card.Text collapsible>
-              The point of seeking purpose is flourishing (or as researchers sometimes say, eudaemonic well-being).  It turns out that flourishing does not require the transient state of conventional happiness.  In fact, seeking happiness can divert us from the search for purpose.  But if you find purpose, happiness, and more importantly life satisfaction, will almost certainly follow.
-            </Card.Text>
-          </Card>
-
-          {/* TIP 6 */}
-          <Card eventKey="6">
-            <Card.Header>
-              <Card.Title>
-                6.	Be aware that your search may be difficult for others to understand or contribute to.
-              </Card.Title>
-            </Card.Header>
-            <Card.Text>
-              <p>In fact, those closest to you may feel threatened, or even blamed, by your desire to seek a change in your life.  Your search for your meaning and purpose is up to you, not others.  So use discretion about who you talk to and what you reveal about your process.  This is especially important early on when your emerging understandings are like young seedlings that are just starting to sprout:  they are easily trampled and impaired by others even when there was no intention to do harm.</p>
-              <p>Remember that a key challenge in finding purpose is correcting an imbalance between “self” and “other.”  Only you can know your circumstances, but you may want to wait until after completing some of the program, and establishing a stronger sense of your self-awareness and autonomy, before you share your intentions more broadly.</p>
-              <p>At some point, you may find yourself ready to share your experiences.  A benefit of being open with someone else is that it may allow you to be more open with yourself.  And by talking through your journey, you may discover further insight.  But this can happen in due time when your insights and self-understandings are securely anchored and established.</p>
-            </Card.Text>
-          </Card>
-
-          {/* TIP 7 */}
-          <Card eventKey="7">
-            <Card.Header>
-              <Card.Title>
-                7.	To go deeper through journaling.
-              </Card.Title>
-            </Card.Header>
-
-            <Card.Text collapsible>
-              <p>Most people find new layers of understanding are revealed when they write about the insights they gain from the exercises. Establishing a regular journaling practice will not only benefit you throughout the MAPmaker program as you are examining your life, but will continue to provide insight and accountability after you complete the program. Establishing a journaling routine now will provide immediate and future rewards.</p>
-              <p>If you’d like a little guidance for this writing, you could respond to one of the following prompts when you sit down to journal:</p>
-              <ol>
-                <li>What was most surprising or revelatory to you about what you did in the previous exercises? </li>
-                <li>Did you see something about yourself that you hadn’t been aware of before? </li>
-                <li>Did you struggle to articulate a response to any of the prompts? </li>
-                <li>Did you feel any fear, anxiety, or heightened emotion that you can now explore in writing to better understand it? </li>
-                <li>Did you feel any relief, encouragement, hope, or optimism that you can now explore in writing to better understand it?</li>
-              </ol>
-            </Card.Text>
-          </Card>
-
-        </ListGroup>
-
+        <Tips />
 
         <div className="text-center">
         { user && user.login_token ? 
