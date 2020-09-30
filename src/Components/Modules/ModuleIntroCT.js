050dd3f5f18161f3df487387437e7510542dfa8e
Craig Quincy <craig.quincy@techstars.com>
2020-09-30 06:52:05 -0600

    advance from Intro to next section

---
+++ src/Components/Modules/ModuleIntroCT.js
@@ -0,0 +1,48 @@
+import { connect } from 'react-redux'
+import ModuleIntro from './ModuleIntro'
+import { getUser } from '../../store/user/reducer'
+import { sectionCompletedAC } from '../../store/user/actions'
+import { bindActionCreators } from 'redux';
+
+/* *****************************************
+   mapStateToProps()
+
+   passedProps:
+     moduleNum -- integer
+     sectionNum -- integer
+     sectionTitle -- title of the section for redisplay in modal
+     exercise -- component user will interact with
+******************************************** */
+const mapStateToProps = ( state, passedProps ) => {
+  // console.log( "SectionExerciseCT::mapStateToProps()" )
+
+  const { moduleNum } = passedProps
+
+  // get user
+  const user = getUser( state.userRD )
+
+  return {
+    moduleNum,
+    user
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/* *****************************************
+   mapDispatchToProps()
+
+   passedProps -- see mapStateToProps above
+******************************************** */
+const mapDispatchToProps = ( dispatch, passedProps ) => {
+  return {
+    sectionCompletedCB: bindActionCreators(sectionCompletedAC, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)( ModuleIntro )
