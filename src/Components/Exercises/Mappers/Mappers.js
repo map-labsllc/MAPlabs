3315b0b40a172f2646bcc7a3164783da57724fd1
Craig Quincy <craig.quincy@techstars.com>
2020-11-15 17:39:16 -0700

    fix layout

--- src/Components/Exercises/Mappers/Mappers.js
+++ src/Components/Exercises/Mappers/Mappers.js
@@ -19,15 +19,16 @@ import {
     EXERCISE_552,
     EXERCISE_560,
 } from '../../Modules/Module5'
+import { Card } from 'react-bootstrap'
 
 const Mappers = ({ moduleId, sectionId }) => {
   const navIds = {moduleId, sectionId}
 
   return (
     <div>
-      <SectionNav {...navIds} 
+      <SectionNav {...navIds}
         subSections= {[
-          { 
+          {
             id: 565,
             title: `In order to reach my future desired situation`,
             exercise: EXERCISE_565
@@ -35,120 +36,142 @@ const Mappers = ({ moduleId, sectionId }) => {
         ]}
       />
 
-      <h4>HOW I'LL DO IT</h4>
-      <h5>Goals - I will need to change my beavior</h5>
-      <SectionNav {...navIds} 
-        subSections= {[
-          { 
-            id: 560,
-            title: `Breaking and Building`,
-            exercise: EXERCISE_560
-          },
-        ]}
-      />
+      <div className="background text-left">
+        <Card>
+          <Card.Header>
+            <Card.Title><h4>HOW I'LL DO IT</h4></Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <div className="container-fluid contain"> 
+              <h5>Goals - I will need to change my beavior</h5>
+              <SectionNav {...navIds} 
+                subSections= {[
+                  { 
+                    id: 560,
+                    title: `Breaking and Building`,
+                    exercise: EXERCISE_560
+                  },
+                ]}
+              />
 
-      <h5>Commitments</h5>
-      <SectionNav {...navIds} 
-        subSections= {[
-          { 
-            id: 550,
-            title: `I will need personal growth`,
-            exercise: EXERCISE_550
-          },
-          { 
-            id: 551,
-            title: `I will need supporting relationships`,
-            exercise: EXERCISE_551
-          },
-          { 
-            id: 552,
-            title: `I will need engagement mastery`,
-            exercise: EXERCISE_552
-          },
-        ]}
-      />
+              <h5>Commitments</h5>
+              <SectionNav {...navIds} 
+                subSections= {[
+                  { 
+                    id: 550,
+                    title: `I will need personal growth`,
+                    exercise: EXERCISE_550
+                  },
+                  { 
+                    id: 551,
+                    title: `I will need supporting relationships`,
+                    exercise: EXERCISE_551
+                  },
+                  { 
+                    id: 552,
+                    title: `I will need engagement mastery`,
+                    exercise: EXERCISE_552
+                  },
+                ]}
+              />
 
-      <SectionNav {...navIds} 
-        subSections= {[
-          { 
-            id: 540,
-            title: `My "who" and "what" come together in my life purpose`,
-            exercise: EXERCISE_540
-          },
-        ]}
-      />
+              <SectionNav {...navIds} 
+                subSections= {[
+                  { 
+                    id: 540,
+                    title: `My "who" and "what" come together in my life purpose`,
+                    exercise: EXERCISE_540
+                  },
+                ]}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
 
-      <h4>WHAT I CARE ABOUT</h4>
-      <SectionNav {...navIds} 
-        subSections= {[
-          { 
-            id: 531,
-            title: `These are my thoughts on "what" I care about`,
-            exercise: EXERCISE_531
-          },
-        ]}
-      />
-      <h5>Focus Beyond Self</h5>
-      <SectionNav {...navIds} 
-        sectionId={sectionId}
-        moduleId={moduleId}
-        subSections= {[
-          { 
-            id: 531,
-            title: `These are my thoughts on "what" I care about`,
-            exercise: EXERCISE_531
-          },
-          { 
-            id: 530,
-            title: `I want to focus on something beyond myself`,
-            exercise: EXERCISE_530
-          },
-          { 
-            id: 512,
-            title: `I become a purpose-seeking person "who"`,
-            exercise: EXERCISE_512
-          },
-        ]}
-      />
+      <div className="background text-left">
+        <Card>
+          <Card.Header>
+            <Card.Title><h4>WHAT I CARE ABOUT</h4></Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <div className="container-fluid contain">
+              <h5>Focus Beyond Self</h5>
+              <SectionNav {...navIds} 
+                sectionId={sectionId}
+                moduleId={moduleId}
+                subSections= {[
+                  { 
+                    id: 531,
+                    title: `These are my thoughts on "what" I care about`,
+                    exercise: EXERCISE_531
+                  },
+                  { 
+                    id: 530,
+                    title: `I want to focus on something beyond myself`,
+                    exercise: EXERCISE_530
+                  },
+                  { 
+                    id: 512,
+                    title: `I become a purpose-seeking person "who"`,
+                    exercise: EXERCISE_512
+                  },
+                ]}
+              />
+            </div>
+          </Card.Body>  
+        </Card>
+      </div>
 
-      <h4>WHO AM I</h4>
-      <h5>My Meaning</h5>
-      <SectionNav {...navIds} 
-        sectionId={sectionId}
-        moduleId={moduleId}
-        subSections= {[
-          { 
-            id: 520,
-            title: `By grounding myself in meaning`,
-            exercise: EXERCISE_520
-          },
-          { 
-            id: 522,
-            title: `By using my top strengths`,
-            exercise: EXERCISE_522
-          },
-          { 
-            id: 521,
-            title: `By living out my deepest desires`,
-            exercise: EXERCISE_521
-          },
-        ]}
-      />
+      <div className="background text-left">
+        <Card>
+          <Card.Header>
+            <Card.Title><h4>WHO AM I</h4></Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <div className="container-fluid contain">
+              <h5>My Meaning</h5>
+              <SectionNav {...navIds} 
+                sectionId={sectionId}
+                moduleId={moduleId}
+                subSections= {[
+                  { 
+                    id: 520,
+                    title: `By grounding myself in meaning`,
+                    exercise: EXERCISE_520
+                  },
+                  { 
+                    id: 522,
+                    title: `By using my top strengths`,
+                    exercise: EXERCISE_522
+                  },
+                  { 
+                    id: 521,
+                    title: `By living out my deepest desires`,
+                    exercise: EXERCISE_521
+                  },
+                ]}
+              />
 
-      <SectionNav {...navIds} 
-        subSections= {[
-          {
-            id: 511,
-            title: `I need to acknowledge these important themes`,
-            exercise: EXERCISE_511
-          },
-          {
-            id: 510,
-            title: `In order to improve my current situation`,
-            exercise: EXERCISE_510
-          }
-        ]}
-      />
+              <SectionNav {...navIds} 
+                subSections= {[
+                  {
+                    id: 511,
+                    title: `I need to acknowledge these important themes`,
+                    exercise: EXERCISE_511
+                  },
+                  {
+                    id: 510,
+                    title: `In order to improve my current situation`,
+                    exercise: EXERCISE_510
+                  }
+                ]}
+              />
+            </div>
+          </Card.Body>  
+        </Card>
+      </div>
+
     </div>
   )
 }
