ac371c351289dfa1869987c1020edd3dc7902f95
Craig Quincy <craig.quincy@techstars.com>
2020-11-16 09:09:00 -0700

    add instructions for M5

--- src/Components/Modules/Module5Text.js
+++ src/Components/Modules/Module5Text.js
@@ -54,30 +54,118 @@ Add to your Dashboard your key themes. At the end of each module you were asked 
 </p>
 <ol>
 <li>Do your themes feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise or edit them to more accurately reflect your current self-knowledge and understanding.</li>
-</li>Note which themes seem most critical to you, and which seem secondary. If your themes seem to overlap, try to consolidate them. Then choose only the most critical themes that you would like to add to your dashboard. Listing too many themes will cause you to lose focus, so try to only choose the most important 1 - 3 themes for your dashboard.</li>
+<li>Note which themes seem most critical to you, and which seem secondary. If your themes seem to overlap, try to consolidate them. Then choose only the most critical themes that you would like to add to your dashboard. Listing too many themes will cause you to lose focus, so try to only choose the most important 1 - 3 themes for your dashboard.</li>
 </ol>`
 
-export const QUES_512_DESC = `Add any summarizing thoughts or directives that you feel will be helpful to you in the future as a quick reference, reminder, or accountability statement about your key themes.`
+export const QUES_512_DESC = `Summarize the context of your purpose-seeking life. Based on your final reflection on your Current Situation and your articulated themes, add any summarizing thoughts, statements, phrases, or directives to your Dashboard about your macro context that you feel will be helpful to you in the future as a quick reference, reminder, or accountability statement.`
 
-export const QUES_520_DESC = `TODO`
+export const QUES_520_DESC = `<p>Add to your Dashboard your key insights about meaning. At the end of each module you were asked to articulate what was most meaningful for you about that module’s subject. Review your responses about what you found most meaningful.</p>
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise or edit them to more accurately reflect your current self-knowledge and understanding.</li>
+  <li>Note which responses seem most critical to you, and which seem secondary. If your responses seem to overlap, try to consolidate them. Then choose only the most critical sources of meaning that you would like to add to your dashboard.</li>
+  </ol>`
 
-export const QUES_521_DESC = `Review your Desires Statement and revise or edit it to more accurately reflect your current self-knowledge and understanding. If possible, consolidate your statement down into a simple, to-the-point synopsis of only the most important desires in a few sentences.`
+export const QUES_521_DESC = `<p>At the end of Module 3 you encapsulated into a Desires Statement what you desire most.</p>
+<ol>
+  <li>Review your Desires Statement. Does your Desires Statement feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave it as is. If not, revise or edit it to more accurately reflect your current self-knowledge and understanding.</li>
+  <li>Note which of your desires seem most compelling to you, and which seem secondary. If your responses seem to overlap, try to consolidate them. Then choose only the most compelling desires that you would like to add to your dashboard.</li>
+</ol>
+`
 
-export const QUES_522_DESC = `TODO`
-export const QUES_523_DESC = `TODO`
-export const QUES_524_DESC = `TODO`
+export const QUES_522_DESC = `Review your 5 top Signature Strengths as revealed to you in the VIA Strengths Survey and consider how they relate to your senses of meaning and your desires. Add only your most important Signature Strengths to your Dashboard (you may also add any other strengths you feel should be considered a Signature Strength, but try not to exceed 5 strengths total).`
 
-export const QUES_530_DESC = `TODO`
-export const QUES_531_DESC = `TODO`
+export const QUES_523_DESC = `Review your “embodiment” themes. Do your themes feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise or edit them to more accurately reflect your current self-knowledge and understanding. Add only the most important embodiment themes to your Dashboard.`
 
-export const QUES_540_DESC = `TODO`
+export const QUES_524_DESC = `Review your “impediment” themes. Do your themes feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise or edit them to more accurately reflect your current self-knowledge and understanding. Add only the most important impediment themes to your Dashboard.`
 
-export const QUES_550_DESC = `Review your personal growth commitments to consider how accurately they reflect your current self-knowledge and understanding. Choose only the 5 most critical themes in column C and copy them into the boxes below. You may also revise the statements to better fit with your current self-knowledge and understanding.`
-export const QUES_551_DESC = `Review your relationships to consider how accurately they reflect your current self-knowledge and understanding. Choose only the 5 most critical relationships. You may also revise the statements to better fit with your current self-knowledge and understanding.`
-export const QUES_552_DESC = `Review your engagement mastery to consider how accurately these themes reflect your current self-knowledge and understanding. Choose only the 5 most critical themes in column O and copy them into the boxes below. You may also revise the statements to better fit with your current self-knowledge and understanding.`
+export const QUES_530_DESC = `<p>At the end of each module you were asked what things beyond yourself could be served through
+that module’s subject. Review your responses about what things beyond yourself could be served.
+</p>
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise, edit, or add new ones to them to more accurately reflect your current self-knowledge and understanding, and what, beyond yourself, that you actually care about.</li>
+  <li>Note which responses seem most critical to you, and which seem secondary. If your responses seem to overlap, try to consolidate them. Then choose the most critical things beyond yourself that you would like to add to your dashboard.</li>
+</ol>`
 
-export const QUES_560_DESC = `Review your responses to how you would like to alter how you engage in these areas. Choose only the 10 most critical changes. You may also revise the statements to better fit with your current self-knowledge and understanding.`
-export const QUES_565_DESC = `Revise and consolidate this statement down into a simple, to-the-point synopsis of only the most important concepts in a few sentences.
+export const QUES_531_DESC = `Add any summarizing thoughts, statements, phrases, or directives to your Dashboard that you feel will be helpful to you in the future as a quick reference, reminder, or accountability statement about what you care about and want to serve that is beyond yourself.`
+
+export const QUES_540_DESC = `<p>Bring the Dashboard elements together into a purpose statement. Our lives are most purposeful when a few things hold true: (1) we know what is most meaningful to us, (2) our desires arise out of this meaning, (3) we fulfill our desires through our personal strengths, and (4) we apply our strengths in service of something or someone beyond ourselves.
+</p>
+<p>
+In modules 1 - 4, you worked through a process of separating yourself from all the extrinsic influences that have fought for your attention all of your life. This allowed you to discover what is most intrinsically meaningful to you. From this place of self- understanding, you then worked through the trade-off exercise to discover what you most deeply desire. Then you took the VIA Survey of Character Strengths to understand which strengths you possess to animate your meaning and accomplish your desires. And all
+along, you considered how your meaning, desires, and strengths could apply to something beyond yourself.
+</p>
+<p>
+Your Dashboard now contains many elements of a purposeful life: the thematic elements that give your life a context, your grounded sense of what is most meaningful to you, your awareness and articulation of what you desire most, a list of your greatest strengths, and your outward-looking understanding of how these things can be channeled toward service beyond yourself. These elements can help you to articulate how to live your life more purposefully.
+</p>
+<p>
+It is now time for you to bring these elements together into a purpose statement.
+</p>
+<p>
+Take some time to consider all of the elements on your Dashboard. Then articulate your own personal life purpose statement based on how who you are can meaningfully connect to what you care about in order to better serve yourself and something beyond yourself. Complete the following statement in the most authentic and precise way you can.</p>`
+
+export const QUES_550_DESC = `<p>Personal growth is the most intrinsic of your commitments because it is focused on how you personally develop and who your personal self becomes. This is why you are starting the how of your purpose with personal growth, because personal growth not only develops you into the person who is qualified to accomplish your purposeful activities, but it also makes your purpose pathway sustainable. A sustainable life purpose is one that you personally love and benefit from because it intrinsically grows you in ways that you desire, and in ways that continually develop you for the next phases of your life of purpose. In this way, a purposeful life becomes regenerating. As you invest yourself in your purpose and in serving something beyond yourself, you also receive back through personal growth and development. Consequently, you will start your commitment process by anchoring on the most intrinsic type of commitments, the commitments to personal growth that are necessary for you to accomplish your stated purpose.
+</p>
+<p>
+At the end of each module you were asked to identify areas of personal growth in that module’s subject. Review your responses about areas of personal growth.
+</p>
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise, edit, or add new ones to them to more accurately reflect your current self-knowledge and understanding.
+  <li>Note which responses seem most critical to you achieving your life purpose statement, and which seem secondary. If your responses seem to overlap, feel free to consolidate them.
+    <ol>
+      <li>Choosethe1-3mostcriticalareasofpersonalgrowththatarenecessary,andto which you are willing to commit, in order to support the development of your life purpose. These commitments will make up your primary commitments in order to focus you on the most critical areas of growth and personal investment. Add them to your Dashboard.</li>
+      <li>If you also have personal growth commitments beyond your top 3 that are critical to fulfilling your purpose, you may attach them to the “Other Commitments” section of your dashboard.</li>
+    </ol>
+  </li>
+</ol>`
+
+
+export const QUES_551_DESC = `<p>Relationships provide the support for our personal growth and the context for fulfilling life purpose. No one is an island, and without the support of positive relationships from friends, family, mentors, teachers, and others, none of us are able to grow in the ways necessary to accomplish our purposes. So your next commitment is to the relationships
+that will support you in your personal growth and that will provide the appropriate context for the fulfillment of your purpose.
+</p>
+<p>
+At the end of each module you were asked what relationships you need to support that module’s subject. Review your responses about relationships.
+</p>
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise, edit, or add new ones to them to more accurately reflect your current self-knowledge and understanding.</li>
+  <li>Note which responses seem most critical not only to achieving your life purpose statement, but also to supporting the personal growth commitments you just chose. If your responses seem to overlap, feel free to consolidate them.
+    <ol>
+      <li>Choose the mostcriticalrelationships(thatyoueithercurrentlyhaveorthatyou need to develop in the future) that are necessary, and to which you are willing to commit, in order to support the areas of personal growth which you previously choose. Attach each relationship to the area of personal growth which it most supports on your dashboard.</li>
+      <li>If there are other relationships (that you either currently have or that you need to develop in the future) that are critical to fulfilling your purpose, but that are not attached to your personal growth, you may attach them to the “Other Commitments” section of your dashboard.</li>
+    </ol>
+  </li>
+</ol>
+`
+
+export const QUES_552_DESC = `
+<p>
+Thus far you have committed to both the personal growth that is necessary for you to accomplish your purpose, and to the relationships that will provide you with the support and context you need to continually grow and serve your purpose. Now you will commit to the ways in which your personal growth and relationships can lead you to engagement mastery, that is, to developing mastery in the skills, capabilities, or areas of expertise and knowledge that you need in order to manifest your purpose through your engagements.
+</p>
+<p>At the end of each module you were asked what engagement mastery you need in order to support that module’s subject. Review your responses about engagement mastery.
+</p>
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise, edit, or add new ones to them to more accurately reflect your current self-knowledge and understanding.</li>
+  <li>Note which responses seem most critical to achieving your life purpose statement, and which seem secondary. If your responses seem to overlap, feel free to consolidate them.
+    <ol>
+      <li>Choosethemostcriticalengagementmasteriesthatarenecessary,andtowhich you are willing to commit, in order to support your previously chosen commitments to personal growth and relationships. Attach each mastery to the area of personal growth which it most supports on your dashboard.</li>
+      <li>If there are other masteries that are critical to fulfilling your purpose, you may attach them to the “Other Commitments” section of your dashboard</li>
+    </ol>
+  </li>
+  </ol>
+`
+
+export const QUES_560_DESC = `<p>
+</p>At the end of each module you were asked what you needed to break from and build to with regard to that module’s subject: feelings and experiences, values and beliefs, desires, and strengths. Review your responses about what you wanted break from and build toward.
+<ol>
+  <li>Do your responses feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave them as they are. If not, revise, edit, or add new ones to them to more accurately reflect your current self-knowledge and understanding.</li>
+  <li>Note which responses seem most critical to achieving your life purpose statement, and which seem secondary. If your responses seem to overlap, feel free to consolidate them. Then choose the most critical break from and build to goals that are necessary in
+  order to support the achievement of your life purpose, and attach them to your final commitments on your dashboard.
+  </li>
+</ol>`
+
+export const QUES_565_DESC = `
+<p>
+At the very beginning of the MAPmaker process you wrote a Future Desired Situation statement articulating what you most desired in your future. Now that you have completed nearly all of the MAPmaker program, it is time to revisit this statement to see how it stands up to the learning you have encountered. Reread the statement and make sure that it encompasses what you have learned through all five modules, and that it accurately reflects all of your understanding at this point. Does your description of your Future Desired Situation feel accurate, authentic, comprehensive, and empowering to you? If so, feel free to leave it as it is. If not, revise or edit it to more accurately reflect your current self-knowledge and understanding. Since this statement will now become part of your “quick-reference” dashboard, try to consolidate it down into a simple, to-the-point synopsis of only the most important concepts in a few sentences.
+</p>
 `
 
 export const QUES_570_DESC = ``
