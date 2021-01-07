a96bed1bf3716d705c02f7942b4d6c44c8c7ac09
Nathaniel Birne <nbirne71@gmail.com>
2021-01-07 15:09:36 -0500

    remove paragraph elements from M4 intro

--- src/Components/Modules/Module4Text.js
+++ src/Components/Modules/Module4Text.js
@@ -1,22 +1,27 @@
-export const MOD_4_DESC = `
-<p>Module 4 of MAPmaker is a turning point.</p> 
-<p>In Module 1 you assessed your current situation and dared to envision a more positive future for yourself.  In Module 2 you inventoried all of your influences and engaged in thoughtful self-reflection to differentiate your personal values and beliefs from those of others around you. In Module 3 you delved deeply into your most authentic self to acknowledge and embrace your greatest personal desires.</p> 
-<p>You must be exhausted! (But hopefully you are also enlivened with self-awareness, honesty, curiosity, and an enlarged sense of your personal possibility).</p> 
-<p>In this module we will transition from deep, inwardly focused introspection, to begin focusing on how our authentic self can more optimally engage in the outward world around us. We will do this by examining the <i>Environmental mastery</i> element of the MAPPERS model of well-being.</p>
-<p>Environmental mastery occurs when we know our personal values, beliefs, desires, and strengths, and we are able to manage and master our environments in order to effectively work with, and impact, the opportunities surrounding us. This means that we take all of the thoughtful self-reflection and discovery about our <i>values</i>, <i>beliefs</i>, and <i>desires</i>, and add to it an awareness of our personal <i>strengths</i>, that is, our aptitudinal orientations that come most naturally to us.</p>
-<p>We all have core strengths and talents. But if we are not aware of them, our circumstances and habits can lead us to not act on some of our best attributes in our everyday lives. As we have already seen with our values, beliefs, and desires, it is common to find ourselves striving after things that others want for us or have influenced us to want. When we do so, it often causes us to be in situations where we use skills that may be highly esteemed, but that are not our core strengths. In fact, it is not uncommon for us to end up in jobs, careers, and other life situations that primarily demand our weaknesses or our middle strengths, rather than our core “signature” strengths. All the while, our true strengths lie dormant.</p>
-<p>In the following exercises we will start by taking the VIA Survey of Character Strengths to discover our “signature strengths,” before examining the situations in our lives that embody and impede these strengths. Using this self-knowledge, we will consider how we can cultivate situations and specific opportunities to leverage our personal strengths in ways that allow both ourselves and the others we serve to flourish.</p>
+export const MOD_4_DESC = `Module 4 of MAPmaker is a turning point.
+<br><br> 
+In Module 1 you assessed your current situation and dared to envision a more positive future for yourself.  In Module 2 you inventoried all of your influences and engaged in thoughtful self-reflection to differentiate your personal values and beliefs from those of others around you. In Module 3 you delved deeply into your most authentic self to acknowledge and embrace your greatest personal desires. 
+<br><br>
+You must be exhausted! (But hopefully you are also enlivened with self-awareness, honesty, curiosity, and an enlarged sense of your personal possibility).
+<br><br>
+In this module we will transition from deep, inwardly focused introspection, to begin focusing on how our authentic self can more optimally engage in the outward world around us. We will do this by examining the <i>Environmental mastery</i> element of the MAPPERS model of well-being.
+<br><br>
+Environmental mastery occurs when we know our personal values, beliefs, desires, and strengths, and we are able to manage and master our environments in order to effectively work with, and impact, the opportunities surrounding us. This means that we take all of the thoughtful self-reflection and discovery about our <i>values</i>, <i>beliefs</i>, and <i>desires</i>, and add to it an awareness of our personal <i>strengths</i>, that is, our aptitudinal orientations that come most naturally to us.
+<br><br>
+We all have core strengths and talents. But if we are not aware of them, our circumstances and habits can lead us to not act on some of our best attributes in our everyday lives. As we have already seen with our values, beliefs, and desires, it is common to find ourselves striving after things that others want for us or have influenced us to want. When we do so, it often causes us to be in situations where we use skills that may be highly esteemed, but that are not our core strengths. In fact, it is not uncommon for us to end up in jobs, careers, and other life situations that primarily demand our weaknesses or our middle strengths, rather than our core “signature” strengths. All the while, our true strengths lie dormant.
+<br><br>
+In the following exercises we will start by taking the VIA Survey of Character Strengths to discover our “signature strengths,” before examining the situations in our lives that embody and impede these strengths. Using this self-knowledge, we will consider how we can cultivate situations and specific opportunities to leverage our personal strengths in ways that allow both ourselves and the others we serve to flourish.
 `
 
 // TODO... This is needs to be updated
-export const QUES_410_DESC = 'Determine your top 5 strengths by taking the  <a href="https://www.authentichappiness.sas.upenn.edu/" target="_blank">VIA Survey of Character Strengths</a>. Enter your top 5 strengths using the drop down menu in the order they are reported in the VIA Survey.'
+export const QUES_410_DESC = `Determine your top 5 strengths by taking the  <a href="https://www.authentichappiness.sas.upenn.edu/" target="_blank">VIA Survey of Character Strengths</a>. Enter your top 5 strengths using the drop down menu in the order they are reported in the VIA Survey.`
 
-export const QUES_420_DESC = 'For each strength write a simple word or phrase that describes embodiment or impediment for that strength. List as many as you\'d like for each. Define each sitation as an Embodiment (if you were able) or Impediment (if you were unable).'
+export const QUES_420_DESC = `For each strength write a simple word or phrase that describes embodiment or impediment for that strength. List as many as you'd like for each. Define each sitation as an Embodiment (if you were able) or Impediment (if you were unable).`
 
-export const QUES_430_DESC = 'Select only your most important 5 embodiments from the previous exercise'
+export const QUES_430_DESC = `Select only your most important 5 embodiments from the previous exercise`
 
-export const QUES_440_DESC = 'Select only your most important 5 impediments from the previous Exercise.'
+export const QUES_440_DESC = `Select only your most important 5 impediments from the previous Exercise.`
 
-export const QUES_450_DESC = 'Reflect on themes evident in your “embodiments” and “impediments”.'
+export const QUES_450_DESC = `Reflect on themes evident in your “embodiments” and “impediments”.`
 
-export const QUES_460_DESC = 'In order to create a more meaningful and purposeful life, I want to increase my Environmental mastery by altering how I engage with my strengths, embodiments, impediments and goals.  Determine how to "break and build" in each of these categories.'
+export const QUES_460_DESC = `In order to create a more meaningful and purposeful life, I want to increase my Environmental mastery by altering how I engage with my strengths, embodiments, impediments and goals.  Determine how to "break and build" in each of these categories.`
